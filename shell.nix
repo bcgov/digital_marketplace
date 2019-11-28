55e978080d0aae40aa636c8067f8e728e725b33a
Dhruv Dang <hi@dhruv.io>
2019-11-28 14:46:31 -0800

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -5,8 +5,7 @@ with pkgs;
 mkShell rec {
   #uncomment the next line and comment the line after that
   #if you want to use docker with nix.
-  #buildInputs = [ nodejs-10_x sass docker docker_compose postgresql_10 awscli ];
-  buildInputs = [ nodejs-10_x sass postgresql_10 awscli ];
+  buildInputs = [ nodejs-10_x sass postgresql_10 docker_compose docker ];
   shellHook = ''
     source ~/.bashrc
     npm install
