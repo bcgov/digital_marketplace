99c00190fecb2934adcd2deb2bfb6b1d5408d950
Jesse <jesse.hughes.it@gmail.com>
2019-12-03 19:16:02 -0800

    Better layout for vertical-bar/menu.tsx

--- src/front-end/typescript/lib/pages/list-sidebar.tsx
+++ src/front-end/typescript/lib/pages/list-sidebar.tsx
@@ -2,6 +2,7 @@ import { makePageMetadata } from 'front-end/lib';
 import { Route, SharedState } from 'front-end/lib/app/types';
 import { ComponentView, GlobalComponentMsg, PageComponent, PageInit, Update } from 'front-end/lib/framework';
 import Link from 'front-end/lib/views/link';
+import Icon from 'front-end/lib/views/icon';
 import makeVerticalBar from 'front-end/lib/views/vertical-bar/menu';
 import React from 'react';
 import { Col, Row } from 'reactstrap';
@@ -29,14 +30,13 @@ const view: ComponentView<State, Msg> = ({ state }) => {
     <div>
       <Row className='mb-3 pb-3'>
         <Col xs='12'>
-          Hello, world.
+          <h1>Hello</h1>
+          <p> World </p>
         </Col>
       </Row>
     </div>
   );
 };
-
-// const menuLinks: Link<Params>[] = [ <Link>LinkTitle!</Link> ]
 
 export const component: PageComponent<RouteParams, SharedState, State, Msg> = {
   init,
@@ -44,7 +44,9 @@ export const component: PageComponent<RouteParams, SharedState, State, Msg> = {
   view,
   viewVerticalBar: makeVerticalBar<State, Msg>({
     links: [
-      <Link>hi!</Link>
+      <Link button color='primary'><Icon name='search' className='pr-1'></Icon>Profile</Link>,
+      <Link button><Icon name='search' className='pr-1'></Icon>Notifications</Link>,
+      <Link button><Icon name='search' className='pr-1'></Icon>Accepted Policies, Terms & Agreements</Link>
     ]
   }),
   getMetadata() {
--- src/front-end/typescript/lib/views/vertical-bar/menu.tsx
+++ src/front-end/typescript/lib/views/vertical-bar/menu.tsx
@@ -1,4 +1,6 @@
 import { ComponentView, ComponentViewProps, View } from 'front-end/lib/framework';
+// import Link from 'front-end/lib/views/link';
+// import Icon from 'front-end/lib/views/icon';
 import  React from 'react';
 
 interface Params<State, Msg> {
@@ -6,15 +8,23 @@ interface Params<State, Msg> {
   backMsg?:      Msg;
   title?:        string;
   description?:  string;
-  links?:        JSX.Element[];
+  links:        JSX.Element[];
 }
 
 function makeVerticalBar<State, Msg, Props extends ComponentViewProps<State, Msg> = ComponentViewProps<State, Msg>> (params: Params<State, Msg>): View<Props>
 {
   return props => {
     return (
-      <div>
-        { params.links }
+      <div className='d-flex flex-column pt-5 position-relative'>
+        {
+          params.links.map( link => {
+            return(
+              <div>
+                {link}
+              </div>
+            )
+          })
+        }
       </div>
     )
   }
