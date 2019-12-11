e33d38fd000e3adf8945cf06f59efad6ab1df2d9
Jesse Hughes <jesse.hughes.it@gmail.com>
2019-12-11 15:51:01 -0800

    DM-127 Stub out user table on user/list

--- src/front-end/typescript/lib/pages/user/list.tsx
+++ src/front-end/typescript/lib/pages/user/list.tsx
@@ -1,33 +1,86 @@
 import { makePageMetadata } from 'front-end/lib';
 import { Route, SharedState } from 'front-end/lib/app/types';
-import { ComponentView, GlobalComponentMsg, PageComponent, PageInit, Update } from 'front-end/lib/framework';
+import * as Table from 'front-end/lib/components/table';
+import { ComponentView, GlobalComponentMsg, immutable, Immutable, mapComponentDispatch, PageComponent, PageInit, Update, updateComponentChild } from 'front-end/lib/framework';
 import React from 'react';
 import { Col, Row } from 'reactstrap';
 import { ADT } from 'shared/lib/types';
 
 export interface State {
-  empty: true;
+  table: Immutable<Table.State>;
 }
 
-export type Msg = GlobalComponentMsg<ADT<'noop'>, Route>;
+type InnerMsg = ADT<'table', Table.Msg>;
+
+export type Msg = GlobalComponentMsg<InnerMsg, Route>;
 
 export type RouteParams = null;
 
 const init: PageInit<RouteParams, SharedState, State, Msg> = async () => ({
-  empty: true,
-  hello: ''
+  table: immutable(await Table.init({
+    idNamespace: 'user-list-table'
+  })),
 });
 
 const update: Update<State, Msg> = ({ state, msg }) => {
-  return [state];
+  switch (msg.tag) {
+    case 'table':
+      return updateComponentChild({
+        state,
+        childStatePath: ['table'],
+        childUpdate: Table.update,
+        childMsg: msg.value,
+        mapChildMsg: value => ({ tag: 'table', value })
+      });
+    default:
+      return [state];
+  }
 };
 
-const view: ComponentView<State, Msg> = ({ state }) => {
+function tableHeadCells(state: Immutable<State>): Table.HeadCells {
+  return [
+    { children: 'Status' },
+    { children: 'Account Type' },
+    { children: 'Name' },
+    { children: 'Admin?' },
+  ];
+}
+
+
+function tableBodyRows(state: Immutable<State>): Table.BodyRows {
+  return [
+    [
+      { children: 'status' },
+      { children: 'type' },
+      { children: 'name' },
+      { children: 'admin' },
+    ],
+    [
+      { children: 'status' },
+      { children: 'type' },
+      { children: 'name' },
+      { children: 'admin' },
+    ],
+    [
+      { children: 'status' },
+      { children: 'type' },
+      { children: 'name' },
+      { children: 'admin' },
+    ],
+  ];
+}
+
+const view: ComponentView<State, Msg> = ({ state, dispatch }) => {
+  const dispatchTable = mapComponentDispatch<Msg, Table.Msg>(dispatch, value => ({ tag: 'table', value }));
   return (
     <div>
       <Row className='mb-3 pb-3'>
         <Col xs='12'>
-          <h1>User List</h1>
+          <Table.view
+            headCells={tableHeadCells(state)}
+            bodyRows={tableBodyRows(state)}
+            state={state.table}
+            dispatch={dispatchTable} />
         </Col>
       </Row>
     </div>
