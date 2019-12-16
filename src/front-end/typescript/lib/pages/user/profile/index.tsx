a7aff2768113aaf7bd46d2d0f3dfc3a5f1a4ab7d
Jesse <jesse.hughes.it@gmail.com>
2019-12-15 16:15:56 -0800

    user/profile/index template update to match design

--- src/front-end/typescript/lib/pages/user/profile/index.tsx
+++ src/front-end/typescript/lib/pages/user/profile/index.tsx
@@ -2,13 +2,15 @@ import { makePageMetadata } from 'front-end/lib';
 import { Route, SharedState } from 'front-end/lib/app/types';
 import { ComponentView, GlobalComponentMsg, Immutable, immutable, mapComponentDispatch, PageComponent, PageInit, Update, updateComponentChild } from 'front-end/lib/framework';
 import * as GovProfileForm from 'front-end/lib/pages/user/components/profile';
+import Link from 'front-end/lib/views/link';
 import makeSidebar from 'front-end/lib/views/sidebar/menu';
 import React from 'react';
 import { Col, Row } from 'reactstrap';
-import { getAllUsers } from 'shared/lib/resources/user';
+import { getAllUsers, User, viewStringFor } from 'shared/lib/resources/user';
 import { adt, ADT } from 'shared/lib/types';
 
 export interface State {
+  user: User;
   govProfile: Immutable<GovProfileForm.State>;
 }
 
@@ -28,6 +30,7 @@ export interface RouteParams {
 }
 
 const init: PageInit<RouteParams, SharedState, State, Msg> = async () => ({
+  user: getAllUsers()[0],
   govProfile: immutable(await GovProfileForm.init({
     existingUser: getAllUsers()[0]
   }))
@@ -53,9 +56,24 @@ const view: ComponentView<State, Msg> = ({ state, dispatch }) => {
     <div>
       <Row className='mb-3 pb-3'>
         <Col xs='12'>
-          <h1>User Profile</h1>
+          <h1>{`${state.user.name}`}</h1>
         </Col>
       </Row>
+      <Row>
+        <Col xs='12' className='my-3 py-3'>
+          <span className='pr-4'><strong>Account Type</strong></span> <span>{`${viewStringFor(state.user.type)}`}</span>
+        </Col>
+      </Row>
+
+      <Row className='border-top my-3 py-3'>
+        <Col xs='2'>
+        </Col>
+        <Col xs='10'>
+          <div className='pb-3'><strong>Profile Picture</strong></div>
+          <Link button className='btn-secondary'>Choose Image</Link>
+        </Col>
+      </Row>
+
       <Row>
         <Col xs='12'>
           <GovProfileForm.view
@@ -63,6 +81,7 @@ const view: ComponentView<State, Msg> = ({ state, dispatch }) => {
             dispatch={mapComponentDispatch(dispatch, value => adt('govProfile' as const, value))} />
         </Col>
       </Row>
+
     </div>
   );
 };
@@ -100,6 +119,6 @@ export const component: PageComponent<RouteParams, SharedState, State, Msg> = {
     ])
   },
   getMetadata() {
-    return makePageMetadata('User Edit');
+    return makePageMetadata('User Profile');
   }
 };
