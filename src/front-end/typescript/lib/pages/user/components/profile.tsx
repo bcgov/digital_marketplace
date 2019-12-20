c9f8aea858637042082337c211c9227077e6f4ba
Jesse <jesse.hughes.it@gmail.com>
2019-12-20 09:10:50 -0800

    Add IDIR to user form

--- src/front-end/typescript/lib/pages/user/components/profile.tsx
+++ src/front-end/typescript/lib/pages/user/components/profile.tsx
@@ -1,11 +1,11 @@
 import * as FormField from 'front-end/lib/components/form-field';
-import * as Checkbox from 'front-end/lib/components/form-field/checkbox';
 import * as ShortText from 'front-end/lib/components/form-field/short-text';
 import { ComponentViewProps, immutable, Immutable, Init, mapComponentDispatch, Update, updateComponentChild, View } from 'front-end/lib/framework';
 import React from 'react';
 import { Col, Row } from 'reactstrap';
-import { getBoolean, getString} from 'shared/lib';
-import { parseUserStatus, UpdateRequestBody, User, UserStatus } from 'shared/lib/resources/user';
+import { getString} from 'shared/lib';
+import { parseUserStatus, User, UserStatus, UserType } from 'shared/lib/resources/user';
+import { Id } from 'shared/lib/types';
 import { adt, ADT } from 'shared/lib/types';
 import { validateEmail, validateName } from 'shared/lib/validation/user';
 
@@ -13,33 +13,39 @@ export interface Params {
   existingUser?: User;
 }
 
-export interface State {
+export interface State extends Params {
   name: Immutable<ShortText.State>;
   email: Immutable<ShortText.State>;
   jobTitle: Immutable<ShortText.State>;
   userStatus: Immutable<ShortText.State>;
-  notificationsOn: Immutable<Checkbox.State>;
+  idpUsername: Immutable<ShortText.State>;
 }
 
 export type Msg =
-  ADT<'jobTitle',        ShortText.Msg> |
-  ADT<'email',           ShortText.Msg> |
-  ADT<'userStatus',      ShortText.Msg> |
-  ADT<'name',            ShortText.Msg> |
-  ADT<'notificationsOn', Checkbox.Msg>
+  ADT<'jobTitle',     ShortText.Msg> |
+  ADT<'email',        ShortText.Msg> |
+  ADT<'userStatus',   ShortText.Msg> |
+  ADT<'name',         ShortText.Msg> |
+  ADT<'idpUsername',  ShortText.Msg>
   ;
 
-export type Values = Required<UpdateRequestBody>;
+export interface Values {
+    id: Id;
+    status: UserStatus;
+    name: string;
+    email: string;
+    avatarImageFile: string;
+    idpUsername: string;
+}
 
 export function getValues(state: Immutable<State>): Values {
   return {
     id: FormField.getValue(state.name),
-    status: parseUserStatus(FormField.getValue(state.userStatus)) || UserStatus.Active,
+    status: parseUserStatus(FormField.getValue(state.userStatus)) || UserStatus.Active, // TODO(Jesse): This seems a bit janky..
     name: FormField.getValue(state.name),
     email: FormField.getValue(state.email),
     avatarImageFile: FormField.getValue(state.name),
-    notificationsOn: FormField.getValue(state.notificationsOn),
-    acceptedTerms: false // TODO(Jesse): Implement
+    idpUsername: FormField.getValue(state.idpUsername)
   };
 }
 
@@ -67,11 +73,13 @@ export function setErrors(state: Immutable<State>, errors: Errors): Immutable<St
 
 export const init: Init<Params, State> = async ({ existingUser }) => {
   return {
-    notificationsOn: immutable(await Checkbox.init({
+    existingUser,
+    idpUsername: immutable(await ShortText.init({
       errors: [],
       child: {
-        value: getBoolean(existingUser, 'notificationsOn'),
-        id: 'user-gov-notifications-on'
+        type: 'text',
+        value: getString(existingUser, 'idpUsername'),
+        id: 'user-gov-idir-username'
       }
     })),
     jobTitle: immutable(await ShortText.init({
@@ -115,13 +123,13 @@ export const init: Init<Params, State> = async ({ existingUser }) => {
 
 export const update: Update<State, Msg> = ({ state, msg }) => {
   switch (msg.tag) {
-    case 'notificationsOn':
+    case 'idpUsername':
       return updateComponentChild({
         state,
-        childStatePath: ['notificationsOn'],
-        childUpdate: Checkbox.update,
+        childStatePath: ['idpUsername'],
+        childUpdate: ShortText.update,
         childMsg: msg.value,
-        mapChildMsg: (value) => adt('notificationsOn', value)
+        mapChildMsg: (value) => adt('idpUsername', value)
       });
     case 'jobTitle':
       return updateComponentChild({
@@ -162,12 +170,28 @@ export interface Props extends ComponentViewProps<State, Msg> {
   disabled?: boolean;
 }
 
+function isPublic(user: User | undefined): boolean {
+  const result = (user !== undefined) && (user.type === UserType.Government || user.type === UserType.Admin);
+  return result;
+}
+
 export const view: View<Props> = props => {
   const { state, dispatch, disabled } = props;
   return (
     <div>
       <Row>
         <Col xs='12'>
+
+          { isPublic(state.existingUser) ?
+            <ShortText.view
+              extraChildProps={{} /* TODO(Jesse): How do we add the ? helptext icon to this element? */ }
+              label='IDIR'
+              disabled={true}
+              state={state.idpUsername}
+              dispatch={mapComponentDispatch(dispatch, value => adt('idpUsername' as const, value))} />
+          :
+            null
+          }
 
           <ShortText.view
             extraChildProps={{}}
@@ -192,13 +216,6 @@ export const view: View<Props> = props => {
             disabled={disabled}
             state={state.email}
             dispatch={mapComponentDispatch(dispatch, value => adt('email' as const, value))} />
-
-          <Checkbox.view
-            extraChildProps={{inlineLabel: ''}}
-            label='Recieve Notifications?'
-            disabled={disabled}
-            state={state.notificationsOn}
-            dispatch={mapComponentDispatch(dispatch, value => adt('notificationsOn' as const, value))} />
 
         </Col>
       </Row>
