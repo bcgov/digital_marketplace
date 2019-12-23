5f48b0ccfb36c620f1f2467ea18fc224c2670917
Jesse <jesse.hughes.it@gmail.com>
2019-12-23 15:49:34 -0800

    Add CrudResource interface

--- src/shared/lib/resources/organization.ts
+++ src/shared/lib/resources/organization.ts
@@ -1,4 +1,3 @@
-import { create, readMany, readOne, update } from 'shared/lib/http';
 import { FileRecord } from 'shared/lib/resources/file';
 import { User } from 'shared/lib/resources/user';
 import { Id } from 'shared/lib/types';
@@ -44,22 +43,6 @@ export type UpdateRequestBody = Partial<CreateRequestBody>;
 export interface UpdateValidationErrors extends ErrorTypeFrom<UpdateRequestBody> {
   id?: string[];
   permissions?: string[];
-}
-
-export async function readOneOrganization(id: string): Promise<Organization | null> {
-  return readOne<Organization>(`/api/organizations/${id}`);
-}
-
-export async function readAllOrganizations(): Promise<Organization[]> {
-  return readMany<Organization>('/api/organizations');
-}
-
-export async function createOrganization(requestBody: CreateRequestBody): Promise<Organization | null> {
-  return create<Organization, CreateRequestBody>('/api/organizations', requestBody);
-}
-
-export async function updateOrganization(id: Id, requestBody: UpdateRequestBody): Promise<Organization | null> {
-  return update<Organization, UpdateRequestBody>(`/api/organizations/${id}`, requestBody);
 }
 
 export function Empty(): Organization {
