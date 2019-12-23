161bc6bb5bfe77b5c1ef8bb5cbccf79d30580518
Jesse <jesse.hughes.it@gmail.com>
2019-12-23 12:45:25 -0800

    Make generic CRUD functions

--- src/shared/lib/resources/organization.ts
+++ src/shared/lib/resources/organization.ts
@@ -1,6 +1,5 @@
 import { request } from 'shared/lib/http';
 import { PublicFile } from 'shared/lib/resources/file';
-import * as OrgResource from 'shared/lib/resources/organization';
 import { User } from 'shared/lib/resources/user';
 import { ClientHttpMethod } from 'shared/lib/types';
 import { Id } from 'shared/lib/types';
@@ -42,48 +41,66 @@ export type CreateValidationErrors = ErrorTypeFrom<CreateRequestBody>;
 export type UpdateRequestBody      = Partial<CreateRequestBody> & { id: Id; };
 export type UpdateValidationErrors = ErrorTypeFrom<UpdateRequestBody>;
 
-export async function readOneOrganization(id: string): Promise<Organization | null> {
-  const response = await request(ClientHttpMethod.Get, `/api/organizations/${id}`);
+
+async function readOne<T>(endpoint: string): Promise<T | null> {
+  const response = await request(ClientHttpMethod.Get, endpoint);
   switch (response.status) {
     case 304:
     case 200:
-      return response.data as Organization;
+      return response.data as T;
     default:
       return null;
   }
 }
 
-export async function readAllOrganizations(): Promise<Organization[]> {
-  const response = await request(ClientHttpMethod.Get, '/api/organizations');
+async function readMany<T>(endpoint: string): Promise<T[]> {
+  const response = await request(ClientHttpMethod.Get, endpoint);
   switch (response.status) {
     case 304:
     case 200:
-      return response.data as Organization[];
+      return response.data as T[];
     default:
       return [];
   }
 }
 
-export async function createOrganization(org: OrgResource.CreateRequestBody): Promise<OrgResource.Organization | null> {
-  const response = await request(ClientHttpMethod.Post, '/api/organizations', org);
+async function create<T, RequestType>(endpoint: string, requestObject: RequestType): Promise<T | null> {
+  const response = await request(ClientHttpMethod.Post, endpoint, requestObject as unknown as object);
   switch (response.status) {
     case 304:
     case 200:
-      return response.data as OrgResource.Organization;
+      return response.data as T;
     default:
       return null;
   }
 }
 
-export async function updateOrganization(org: OrgResource.UpdateRequestBody): Promise<OrgResource.Organization | null> {
-  const response = await request(ClientHttpMethod.Put, `/api/organizations/${org.id}`, org);
+async function update<T, RequestType>(endpoint: string, requestObject: RequestType): Promise<T | null> {
+  const response = await request(ClientHttpMethod.Put, endpoint, requestObject as unknown as object);
   switch (response.status) {
     case 304:
     case 200:
-      return response.data as OrgResource.Organization;
+      return response.data as T;
     default:
       return null;
   }
+}
+
+
+export async function readOneOrganization(id: string): Promise<Organization | null> {
+  return readOne<Organization>(`/api/organizations/${id}`);
+}
+
+export async function readAllOrganizations(): Promise<Organization[]> {
+  return readMany<Organization>('/api/organizations');
+}
+
+export async function createOrganization(org: CreateRequestBody): Promise<Organization | null> {
+  return create<Organization, CreateRequestBody>('/api/organizations', org);
+}
+
+export async function updateOrganization(org: UpdateRequestBody): Promise<Organization | null> {
+  return update<Organization, UpdateRequestBody>(`/api/organizations/${org.id}`, org);
 }
 
 export function Empty(): Organization {
