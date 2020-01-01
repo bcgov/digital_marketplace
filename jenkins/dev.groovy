90ef61d4dc2be8051aa2ce19df6f8ba03e408bd6
Andrew Sutherland <sutherlanda@gmail.com>
2019-12-31 20:28:42 -0700

    Increase build reosurces for openshift
    * Remove useless ZAP scan task

--- jenkins/dev.groovy
+++ jenkins/dev.groovy
@@ -44,29 +44,29 @@ node('maven') {
     }
 }
 
-def owaspPodLabel = "owasp-zap-${UUID.randomUUID().toString()}"
-podTemplate(label: owaspPodLabel, name: owaspPodLabel, serviceAccount: 'jenkins', cloud: 'openshift', containers: [
-  containerTemplate(
-    name: 'jnlp',
-    image: '172.50.0.2:5000/openshift/jenkins-slave-zap',
-    resourceRequestCpu: '500m',
-    resourceLimitCpu: '1000m',
-    resourceRequestMemory: '3Gi',
-    resourceLimitMemory: '4Gi',
-    workingDir: '/home/jenkins',
-    command: '',
-    args: '${computer.jnlpmac} ${computer.name}'
-  )
-]) {
-     stage('ZAP Security Scan') {
-        node(owaspPodLabel) {
-          sleep 60
-          def retVal = sh returnStatus: true, script: '/zap/zap-baseline.py -r baseline.html -t https://dig-mkt-app-dev.pathfinder.gov.bc.ca/'
-          publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: true, reportDir: '/zap/wrk', reportFiles: 'baseline.html', reportName: 'ZAP Baseline Scan', reportTitles: 'ZAP Baseline Scan'])
-          echo "Return value is: ${retVal}"
-        }
-     }
-  }
+// def owaspPodLabel = "owasp-zap-${UUID.randomUUID().toString()}"
+// podTemplate(label: owaspPodLabel, name: owaspPodLabel, serviceAccount: 'jenkins', cloud: 'openshift', containers: [
+//   containerTemplate(
+//     name: 'jnlp',
+//     image: '172.50.0.2:5000/openshift/jenkins-slave-zap',
+//     resourceRequestCpu: '500m',
+//     resourceLimitCpu: '1000m',
+//     resourceRequestMemory: '3Gi',
+//     resourceLimitMemory: '4Gi',
+//     workingDir: '/home/jenkins',
+//     command: '',
+//     args: '${computer.jnlpmac} ${computer.name}'
+//   )
+// ]) {
+//      stage('ZAP Security Scan') {
+//         node(owaspPodLabel) {
+//           sleep 60
+//           def retVal = sh returnStatus: true, script: '/zap/zap-baseline.py -r baseline.html -t https://dig-mkt-app-dev.pathfinder.gov.bc.ca/'
+//           publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: true, reportDir: '/zap/wrk', reportFiles: 'baseline.html', reportName: 'ZAP Baseline Scan', reportTitles: 'ZAP Baseline Scan'])
+//           echo "Return value is: ${retVal}"
+//         }
+//      }
+//   }
 
 // stage('Deploy to Test') {	
 //   timeout(time: 1, unit: 'DAYS') {
