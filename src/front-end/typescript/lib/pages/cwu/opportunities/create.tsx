cfea4728f5fb92b0323a32eecd8b4107e30f3ea2
Jesse <jesse.hughes.it@gmail.com>
2020-01-20 16:31:12 -0800

    Add a bunch more fields

--- src/front-end/typescript/lib/pages/cwu/opportunities/create.tsx
+++ src/front-end/typescript/lib/pages/cwu/opportunities/create.tsx
@@ -1,8 +1,9 @@
 import { makePageMetadata } from 'front-end/lib';
 import { isUserType } from 'front-end/lib/access-control';
 import { Route, SharedState } from 'front-end/lib/app/types';
+import * as LongText from 'front-end/lib/components/form-field/long-text';
 import * as ShortText from 'front-end/lib/components/form-field/short-text';
-import { ComponentView, GlobalComponentMsg, immutable, Immutable, mapComponentDispatch, PageComponent, PageInit, Update } from 'front-end/lib/framework';
+import { ComponentView, GlobalComponentMsg, immutable, Immutable, mapComponentDispatch, PageComponent, PageInit, Update, updateComponentChild } from 'front-end/lib/framework';
 import makeInstructionalSidebar from 'front-end/lib/views/sidebar/instructional';
 import React from 'react';
 import { Col, Row } from 'reactstrap';
@@ -12,10 +13,19 @@ import * as opportunityValidation from 'shared/lib/validation/opportunity';
 
 export interface State {
   title: Immutable<ShortText.State>;
+  teaser: Immutable<LongText.State>;
+  location: Immutable<ShortText.State>;
+  fixedPriceReward: Immutable<ShortText.State>;
+  requiredSkills: Immutable<ShortText.State>;
 }
 
 type InnerMsg
-  = ADT<'title', ShortText.Msg>;
+  = ADT<'title',             ShortText.Msg>
+  | ADT<'teaser',            ShortText.Msg>
+  | ADT<'location',          ShortText.Msg>
+  | ADT<'fixedPriceReward',  ShortText.Msg>
+  | ADT<'requiredSkills',    ShortText.Msg>
+  ;
 
 export type Msg = GlobalComponentMsg<InnerMsg, Route>;
 
@@ -23,6 +33,7 @@ export type RouteParams = null;
 
 async function defaultState() {
   return {
+
     title: immutable(await ShortText.init({
       errors: [],
       validate: opportunityValidation.validateTitle,
@@ -31,7 +42,47 @@ async function defaultState() {
         value: '',
         id: 'opportunity-title'
       }
+    })),
+
+    teaser: immutable(await LongText.init({
+      errors: [],
+      validate: opportunityValidation.validateTitle,
+      child: {
+        value: '',
+        id: 'opportunity-teaser'
+      }
+    })),
+
+    location: immutable(await ShortText.init({
+      errors: [],
+      validate: opportunityValidation.validateTitle,
+      child: {
+        type: 'text',
+        value: '',
+        id: 'opportunity-location'
+      }
+    })),
+
+    fixedPriceReward: immutable(await ShortText.init({
+      errors: [],
+      validate: opportunityValidation.validateFixedPriceAmount,
+      child: {
+        type: 'text',
+        value: '',
+        id: 'opportunity-fixed-price-reward'
+      }
+    })),
+
+    requiredSkills: immutable(await ShortText.init({
+      errors: [],
+      validate: opportunityValidation.validateTitle,
+      child: {
+        type: 'text',
+        value: '',
+        id: 'opportunity-required-skills'
+      }
     }))
+
   };
 }
 
@@ -50,7 +101,56 @@ const init: PageInit<RouteParams, SharedState, State, Msg> = isUserType({
 });
 
 const update: Update<State, Msg> = ({ state, msg }) => {
-  return [state];
+  switch (msg.tag) {
+
+    case 'title':
+      return updateComponentChild({
+        state,
+        childStatePath: ['title'],
+        childUpdate: ShortText.update,
+        childMsg: msg.value,
+        mapChildMsg: (value) => adt('title', value)
+      });
+
+    case 'teaser':
+      return updateComponentChild({
+        state,
+        childStatePath: ['teaser'],
+        childUpdate: LongText.update,
+        childMsg: msg.value,
+        mapChildMsg: (value) => adt('teaser', value)
+      });
+
+    case 'location':
+      return updateComponentChild({
+        state,
+        childStatePath: ['location'],
+        childUpdate: ShortText.update,
+        childMsg: msg.value,
+        mapChildMsg: (value) => adt('location', value)
+      });
+
+    case 'fixedPriceReward':
+      return updateComponentChild({
+        state,
+        childStatePath: ['fixedPriceReward'],
+        childUpdate: ShortText.update,
+        childMsg: msg.value,
+        mapChildMsg: (value) => adt('fixedPriceReward', value)
+      });
+
+    case 'requiredSkills':
+      return updateComponentChild({
+        state,
+        childStatePath: ['requiredSkills'],
+        childUpdate: ShortText.update,
+        childMsg: msg.value,
+        mapChildMsg: (value) => adt('requiredSkills', value)
+      });
+
+    default:
+      return [state];
+  }
 };
 
 const view: ComponentView<State, Msg> = ({ state, dispatch }) => {
@@ -66,6 +166,45 @@ const view: ComponentView<State, Msg> = ({ state, dispatch }) => {
           disabled={disabled}
           state={state.title}
           dispatch={mapComponentDispatch(dispatch, value => adt('title' as const, value))} />
+      </Col>
+
+      <Col xs='12'>
+        <LongText.view
+          extraChildProps={{}}
+          label='Teaser'
+          disabled={disabled}
+          state={state.teaser}
+          dispatch={mapComponentDispatch(dispatch, value => adt('teaser' as const, value))} />
+      </Col>
+
+      <Col xs='12'>
+        <ShortText.view
+          extraChildProps={{}}
+          label='Location'
+          required
+          disabled={disabled}
+          state={state.location}
+          dispatch={mapComponentDispatch(dispatch, value => adt('location' as const, value))} />
+      </Col>
+
+      <Col xs='12'>
+        <ShortText.view
+          extraChildProps={{}}
+          label='Fixed-Price Reward'
+          required
+          disabled={disabled}
+          state={state.fixedPriceReward}
+          dispatch={mapComponentDispatch(dispatch, value => adt('fixedPriceReward' as const, value))} />
+      </Col>
+
+      <Col xs='12'>
+        <ShortText.view
+          extraChildProps={{}}
+          label='Required Skills'
+          required
+          disabled={disabled}
+          state={state.requiredSkills}
+          dispatch={mapComponentDispatch(dispatch, value => adt('requiredSkills' as const, value))} />
       </Col>
 
     </Row>
--- src/shared/lib/validation/opportunity.ts
+++ src/shared/lib/validation/opportunity.ts
@@ -1,5 +1,27 @@
-import { validateGenericString, Validation } from 'shared/lib/validation';
+import { invalid, valid, validateGenericString, Validation } from 'shared/lib/validation';
 
 export function validateTitle(raw: string): Validation<string> {
   return validateGenericString(raw, 'Title');
+}
+
+function validNumberString(n: string): boolean {
+  const result = n.match(/^[0-9,.]*$/);
+  if (result) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validNumber(n: number): boolean {
+  return !isNaN(n);
+}
+
+export function validateFixedPriceAmount(dollarString: string): Validation<string> {
+  const parsedAmount = parseInt(dollarString, 10);
+  if ( validNumberString(dollarString) && validNumber(parsedAmount) && parsedAmount <= 70000) {
+    return valid(dollarString);
+  } else {
+    return invalid(['Please enter a value of $70,000 or less.']);
+  }
 }
