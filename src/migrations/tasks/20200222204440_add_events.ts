9c0b6cf48e9a26a571d967a8ed1366b5637aaf2b
Andrew Sutherland <sutherlanda@gmail.com>
2020-02-22 21:28:24 -0800

    DM-290 database migration

---
+++ src/migrations/tasks/20200222204440_add_events.ts
@@ -0,0 +1,74 @@
+import { makeDomainLogger } from 'back-end/lib/logger';
+import { console as consoleAdapter } from 'back-end/lib/logger/adapters';
+import Knex from 'knex';
+
+enum CWUOpportunityEvent {
+  Edited = 'EDITED',
+  AddendumAdded = 'ADDENDUM_ADDED'
+}
+
+enum CWUProposalEvent {
+  ScoreEntered = 'SCORE_ENTERED'
+}
+
+const logger = makeDomainLogger(consoleAdapter, 'migrations');
+
+export async function up(connection: Knex): Promise<void> {
+  await connection.schema.alterTable('cwuOpportunityStatuses', table => {
+    table.enu('event', Object.values(CWUOpportunityEvent)).nullable();
+    table.string('status').nullable().alter();
+  });
+
+  await connection.schema.raw(`
+    ALTER TABLE "cwuOpportunityStatuses"
+    ADD CONSTRAINT "eitherEventOrStatus" check(
+      ("event" IS NOT NULL AND "status" IS NULL)
+      OR
+      ("event" IS NULL AND "status" IS NOT NULL)
+    )
+  `);
+
+  logger.info('Completed modifying cwuOpportunityStatus table.');
+
+  await connection.schema.alterTable('cwuProposalStatuses', table => {
+    table.enu('event', Object.values(CWUProposalEvent)).nullable();
+    table.string('status').nullable().alter();
+  });
+
+  await connection.schema.raw(`
+    ALTER TABLE "cwuProposalStatuses"
+    ADD CONSTRAINT "eitherEventOrStatus" check(
+      ("event" IS NOT NULL AND "status" IS NULL)
+      OR
+      ("event" IS NULL AND "status" IS NOT NULL)
+    )
+  `);
+
+  logger.info('Completed modifying cwuProposalStatuses table.');
+}
+
+export async function down(connection: Knex): Promise<void> {
+  await connection.schema.raw(`
+    ALTER TABLE "cwuOpportunityStatuses"
+    DROP CONSTRAINT "eitherEventOrStatus"
+  `);
+
+  await connection.schema.alterTable('cwuOpportunityStatuses', table => {
+    table.dropColumn('event');
+    table.string('status').notNullable().alter();
+  });
+
+  logger.info('Completed reverting cwuOpportunityStatuses table.');
+
+  await connection.schema.raw(`
+    ALTER TABLE "cwuProposalStatuses"
+    DROP CONSTRAINT "eitherEventOrStatus"
+  `);
+
+  await connection.schema.alterTable('cwuProposalStatuses', table => {
+    table.dropColumn('event');
+    table.string('status').notNullable().alter();
+  });
+
+  logger.info('Completed reverting cwuProposals table.');
+}
