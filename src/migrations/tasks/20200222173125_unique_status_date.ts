7a4d3aac24e32af6e7080c76577eac60241e0fa4
Andrew Sutherland <sutherlanda@gmail.com>
2020-02-22 23:44:23 -0800

    DM-295 add status to constraint
    - Necessary to allow simultaneous status/event

--- src/migrations/tasks/20200222173125_unique_status_date.ts
+++ src/migrations/tasks/20200222173125_unique_status_date.ts
@@ -6,24 +6,24 @@ const logger = makeDomainLogger(consoleAdapter, 'migrations');
 
 export async function up(connection: Knex): Promise<void> {
   await connection.schema.alterTable('cwuOpportunityStatuses', table => {
-    table.unique(['opportunity', 'createdAt']);
+    table.unique(['opportunity', 'createdAt', 'status']);
   });
   logger.info('Added unique constraint to cwuOpportunityStatuses table.');
 
   await connection.schema.alterTable('cwuProposalStatuses', table => {
-    table.unique(['proposal', 'createdAt']);
+    table.unique(['proposal', 'createdAt', 'status']);
   });
   logger.info('Added unique constraint to cwuOpportunityProposals table.');
 }
 
 export async function down(connection: Knex): Promise<void> {
   await connection.schema.alterTable('cwuOpportunityStatuses', table => {
-    table.dropUnique(['opportunity', 'createdAt']);
+    table.dropUnique(['opportunity', 'createdAt', 'status']);
   });
   logger.info('Removed unique constraint on cwuOpportunityStatuses table.');
 
   await connection.schema.alterTable('cwuProposalStatuses', table => {
-    table.dropUnique(['proposal', 'createdAt']);
+    table.dropUnique(['proposal', 'createdAt', 'status']);
   });
   logger.info('Removed unique constraint on cwuProposalStatuses table.');
 }
