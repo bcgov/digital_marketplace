39654687d99fd017cc9c69584b5b877e2b20af74
Andrew Sutherland <sutherlanda@gmail.com>
2020-03-01 21:01:26 -0800

    DM-231 migration allowing null createdBy on ppsl

---
+++ src/migrations/tasks/20200301202432_cwu_proposal_null_createdby.ts
@@ -0,0 +1,19 @@
+import { makeDomainLogger } from 'back-end/lib/logger';
+import { console as consoleAdapter } from 'back-end/lib/logger/adapters';
+import Knex from 'knex';
+
+const logger = makeDomainLogger(consoleAdapter, 'migrations');
+
+export async function up(connection: Knex): Promise<void> {
+  await connection.schema.alterTable('cwuProposalStatuses', table => {
+    table.uuid('createdBy').nullable().alter();
+  });
+  logger.info('Completed modifying cwuProposalStatuses table.');
+}
+
+export async function down(connection: Knex): Promise<void> {
+  await connection.schema.alterTable('cwuProposalStatuses', table => {
+    table.uuid('createdBy').notNullable().alter();
+  });
+  logger.info('Completed reverting cwuProposalStatuses table.');
+}
