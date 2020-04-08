153959f06f4073f11a98d9529b52f6b33df62e63
Dhruv Dang <hi@dhruv.io>
2020-04-08 16:20:04 -0700

    DM-388 completed swu proposal form

--- src/front-end/typescript/lib/pages/user/lib/index.ts
+++ src/front-end/typescript/lib/pages/user/lib/index.ts
@@ -3,56 +3,7 @@ import { ThemeColor } from 'front-end/lib/types';
 import { fileBlobPath } from 'shared/lib/resources/file';
 import { KeyCloakIdentityProvider, User, UserStatus, UserType, userTypeToKeycloakIdentityProvider } from 'shared/lib/resources/user';
 
-export function isPublic(user: User | undefined): boolean {
-  const result = (user !== undefined) && (user.type === UserType.Government || user.type === UserType.Admin);
-  return result;
-}
-
-type DisplayUserType = 'Public Sector Employee' | 'Vendor';
-
-export interface DisplayUser {
-  name: string;
-  type: DisplayUserType;
-  active: boolean;
-  admin: boolean;
-}
-
-export function getBadgeColor(isActive: boolean): string {
-  return isActive ? 'badge-success' : 'badge-danger';
-}
-
-export function toDisplayUser(user: User): DisplayUser {
-  return ({
-    name: user.name,
-    type: user.type === UserType.Vendor ? 'Vendor' : 'Public Sector Employee',
-    admin: user.type === UserType.Admin ? true : false,
-    active: user.status === UserStatus.Active ? true : false
-  });
-}
-
-export function viewStringForUserStatus(type: UserStatus): string {
-  switch (type) {
-      case UserStatus.InactiveByAdmin:
-      case UserStatus.InactiveByUser:
-        return 'Inactive';
-      case UserStatus.Active:
-        return 'Active';
-  }
-}
-
-export function viewStringForUserType(type: UserType): string {
-  switch (type) {
-      case UserType.Government:
-      case UserType.Admin:
-        return 'Public Sector Employee';
-      case UserType.Vendor:
-        return 'Vendor';
-  }
-}
-
-// Dhruv below:
-
-export function userAvatarPath(user?: User): string {
+export function userAvatarPath(user?: Pick<User, 'avatarImageFile'>): string {
   return user && user.avatarImageFile
     ? fileBlobPath(user.avatarImageFile)
     : DEFAULT_USER_AVATAR_IMAGE_PATH;
