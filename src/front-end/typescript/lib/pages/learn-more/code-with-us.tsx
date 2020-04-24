d6c6d6d98bb35aa05fea0b0d51262b09a175fb60
Andrew Sutherland <sutherlanda@gmail.com>
2020-04-24 16:53:54 -0700

    DM-253 WIP learn more

--- src/front-end/typescript/lib/pages/learn-more/code-with-us.tsx
+++ src/front-end/typescript/lib/pages/learn-more/code-with-us.tsx
@@ -1,8 +1,9 @@
-import { makePageMetadata } from 'front-end/lib';
+import { makePageMetadata, prefixPath } from 'front-end/lib';
 import { Route, SharedState } from 'front-end/lib/app/types';
-import { ComponentView, GlobalComponentMsg, PageComponent, PageInit, Update } from 'front-end/lib/framework';
+import { ComponentView, GlobalComponentMsg, PageComponent, PageInit, Update, View } from 'front-end/lib/framework';
+// import Accordion from 'front-end/lib/views/accordion';
 import React from 'react';
-import { Col, Row } from 'reactstrap';
+import { Col, Container, Row } from 'reactstrap';
 import { ADT } from 'shared/lib/types';
 
 export interface State {
@@ -10,7 +11,8 @@ export interface State {
 }
 
 type InnerMsg
-  = ADT<'noop'>;
+  = ADT<'noop'>
+  | ADT<'toggleAccordion'>;
 
 export type Msg = GlobalComponentMsg<InnerMsg, Route>;
 
@@ -22,26 +24,67 @@ const init: PageInit<RouteParams, SharedState, State, Msg> = async () => ({
 
 const update: Update<State, Msg> = ({ state, msg }) => {
   switch (msg.tag) {
+    case 'toggleAccordion':
+      return [state.update('showAccordion', v => !v)];
     default:
       return [state];
   }
 };
 
+const TitleView: View<{}> = () => {
+  return (
+    <div className='bg-blue-light-alt-2 pt-5'>
+      <Container>
+        <Row>
+          <Col xs='12'>
+            <h1>Code With Us</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs='12' md='8'><em>Code With Us</em> is a procurement mechanism for public sector organizations in British Columbia to pay developers for code.</Col>
+          <Col d-none md='4'><img className='mx-auto d-block mb-n5' src={prefixPath('/images/illustrations/cwu-learnmore.svg')} /></Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+// const VendorView: ComponentView<State, Msg> = ({ state, dispatch }) => {
+//   return (
+//     <Container>
+//       <Accordion
+//       className={''}
+//       toggle={() => dispatch(adt('toggleAccordion', index))}
+//       color='info'
+//       title={title}
+//       titleClassName='h3 mb-0'
+//       icon='vendor'
+//       iconColor={isValid ? undefined : 'warning'}
+//       iconWidth={2}
+//       iconHeight={2}
+//       chevronWidth={1.5}
+//       chevronHeight={1.5}
+//       open={response.isAccordianOpen}>
+//     </Accordion>
+//     </Container>
+//   );
+// };
+
 const view: ComponentView<State, Msg> = ({ state, dispatch }) => {
   return (
-    <Row>
-      <Col xs='12'>
-        Empty Page
-      </Col>
-    </Row>
+    <div>
+      <TitleView />
+      {/* <VendorView /> */}
+    </div>
   );
 };
 
 export const component: PageComponent<RouteParams, SharedState, State, Msg> = {
+  fullWidth: true,
   init,
   update,
   view,
   getMetadata() {
-    return makePageMetadata('Empty Page');
+    return makePageMetadata('Code With Us - Learn More');
   }
 };
