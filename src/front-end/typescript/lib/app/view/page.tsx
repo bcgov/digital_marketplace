2760e716d2798fd4322e9aea6b265c46405a4fba
Dhruv Dang <hi@dhruv.io>
2020-04-28 14:16:33 -0700

    fix vertical spacing for sidebar menu on xs/sm displays

--- src/front-end/typescript/lib/app/view/page.tsx
+++ src/front-end/typescript/lib/app/view/page.tsx
@@ -168,8 +168,8 @@ export function view<RouteParams, PageState, PageMsg>(props: Props<RouteParams, 
           <div className='d-flex flex-column flex-grow-1'>
             <Container className={`position-relative flex-grow-1 d-md-flex flex-md-column align-items-md-stretch ${backgroundClassName}`}>
               <div className={`d-none d-md-block position-absolute bg-${sidebar.color}`} style={{ top: 0, right: '100%', bottom: 0, width: '50vw' }}></div>
-              <Row className='flex-grow-1'>
-                <Col xs='12' md={sidebarColWidth} className={`sidebar bg-${sidebar.color} pr-md-4 pr-lg-5 d-flex flex-column align-items-stretch pt-4 pt-md-6 ${isEmptyOnMobile ? 'pb-md-6' : 'pb-5'}`}>
+              <Row className='flex-grow-1 align-content-start'>
+                <Col xs='12' md={sidebarColWidth} className={`sidebar bg-${sidebar.color} pr-md-4 pr-lg-5 d-flex flex-column align-items-stretch pt-4 pt-md-6 align-self-start ${isEmptyOnMobile ? 'pb-md-6' : 'pb-5'}`}>
                   <ViewAlertsAndBreadcrumbs {...viewAlertsAndBreadcrumbsProps} className='d-md-none' />
                   <sidebar.view {...viewProps} />
                 </Col>
