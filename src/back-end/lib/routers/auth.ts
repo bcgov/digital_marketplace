9a957c1b10a770296793d899dcc38aaadc02a474
Andrew Sutherland <sutherlanda@gmail.com>
2020-04-28 20:13:57 -0700

    Add auth service endpoint for API testing

--- src/back-end/lib/routers/auth.ts
+++ src/back-end/lib/routers/auth.ts
@@ -1,10 +1,11 @@
-import { KEYCLOAK_CLIENT_ID, KEYCLOAK_CLIENT_SECRET, KEYCLOAK_REALM, KEYCLOAK_URL, ORIGIN } from 'back-end/config';
+import { ENV, KEYCLOAK_CLIENT_ID, KEYCLOAK_CLIENT_SECRET, KEYCLOAK_REALM, KEYCLOAK_URL, ORIGIN } from 'back-end/config';
 import { Connection, createSession, createUser, findOneUserByTypeAndUsername, updateUser } from 'back-end/lib/db';
 import { accountReactivatedSelf, userAccountRegistered } from 'back-end/lib/mailer/notifications/user';
-import { makeErrorResponseBody, makeTextResponseBody, nullRequestBodyHandler, Request, Router, TextResponseBody } from 'back-end/lib/server';
+import { makeErrorResponseBody, makeTextResponseBody, nullRequestBodyHandler, passThroughRequestBodyHandler, Request, Router, TextResponseBody } from 'back-end/lib/server';
 import { ServerHttpMethod } from 'back-end/lib/types';
 import { generators, TokenSet, TokenSetParameters } from 'openid-client';
 import qs from 'querystring';
+import { getString, getStringArray } from 'shared/lib';
 import { request as httpRequest } from 'shared/lib/http';
 import { Session } from 'shared/lib/resources/session';
 import { KeyCloakIdentityProvider, User, UserStatus, UserType } from 'shared/lib/resources/user';
@@ -32,7 +33,7 @@ interface KeyCloakTokenRequestData {
 }
 
 async function makeRouter(connection: Connection): Promise<Router<any, any, any, any, TextResponseBody, any, any>> {
-  return [
+  const router: Router<any, any, any, any, TextResponseBody, any, any> = [
     {
       method: ServerHttpMethod.Get,
       path: '/auth/sign-in',
@@ -106,72 +107,11 @@ async function makeRouter(connection: Connection): Promise<Router<any, any, any,
             return makeAuthErrorRedirect(request);
           }
 
-          // Extract claims and create/update user
-          const tokens = new TokenSet(response.data as TokenSetParameters);
-          const claims = tokens.claims();
-          const idpUsername: string | undefined = claims.preferred_username;
-          if (!idpUsername || !tokens.access_token || !tokens.refresh_token) {
-            throw new Error('authentication failure - invalid claims');
+          const tokenSet = new TokenSet(response.data as TokenSetParameters);
+          const { session, existingUser } = await establishSessionWithClaims(connection, request, tokenSet) || {};
+          if (!session) {
+            throw new Error('unable to create session');
           }
-
-          // IDP type is appended to preferred username claim as '@idp-type`
-          let userType: UserType;
-          const identityProvider = idpUsername.substr(idpUsername.lastIndexOf('@') + 1);
-          switch (identityProvider) {
-            case 'idir':
-              userType = UserType.Government;
-              break;
-            case 'github':
-              userType = UserType.Vendor;
-              break;
-            default:
-              request.logger.error('unknown identity provider', makeErrorResponseBody(new Error(identityProvider)));
-              return makeAuthErrorRedirect(request);
-          }
-
-          const dbResult = await findOneUserByTypeAndUsername(connection, userType, idpUsername);
-          if (isInvalid(dbResult)) {
-            makeAuthErrorRedirect(request);
-          }
-          let user = dbResult.value as User | null;
-          const existingUser = !!user;
-          if (!user) {
-            user = getValidValue(await createUser(connection, {
-              type: userType,
-              status: UserStatus.Active,
-              name: claims.name || '',
-              email: claims.email || '',
-              jobTitle: '',
-              idpUsername
-            }), null);
-
-            // If email present, notify of successful account creation
-            if (user && user.email) {
-              userAccountRegistered(user);
-            }
-          } else if (user.status === UserStatus.InactiveByUser) {
-            const { id } = user;
-            const dbResult = await updateUser(connection, { id, status: UserStatus.Active });
-            // Send notification
-            if (isValid(dbResult)) {
-              accountReactivatedSelf(dbResult.value);
-            }
-          } else if (user.status === UserStatus.InactiveByAdmin) {
-            return makeAuthErrorRedirect(request);
-          }
-
-          if (!user) {
-            return makeAuthErrorRedirect(request);
-          }
-
-          const result = await createSession(connection, {
-            accessToken: tokens.refresh_token,
-            user: user.id
-          });
-          if (isInvalid(result)) {
-            return makeAuthErrorRedirect(request);
-          }
-          const session = result.value;
 
           const signinCompleteLocation = redirectOnSuccess ? redirectOnSuccess : (existingUser ? '/dashboard' : '/sign-up/complete');
 
@@ -184,12 +124,128 @@ async function makeRouter(connection: Connection): Promise<Router<any, any, any,
             body: makeTextResponseBody('')
           };
         } catch (error) {
-          request.logger.error('authorization failed', makeErrorResponseBody(error));
+          request.logger.error('authentication failed', makeErrorResponseBody(error));
           return makeAuthErrorRedirect(request);
         }
       })
     }
   ];
+
+  if (ENV === 'development') {
+    router.push({
+      method: ServerHttpMethod.Post,
+      path: '/auth/service',
+      handler: passThroughRequestBodyHandler(async request => {
+        try {
+          // Extract claims and create/update user
+          const validRequestBody = getValidValue(request.body, undefined);
+          if (!validRequestBody) {
+            throw new Error('no request body provided');
+          }
+
+          const body: unknown = validRequestBody.tag === 'json' ? validRequestBody.value : {};
+          const tokens = new TokenSet(body as TokenSetParameters);
+          const { session } = await establishSessionWithClaims(connection, request, tokens) || {};
+          if (!session) {
+            throw new Error('unable to establish session');
+          }
+          return {
+            code: 200,
+            headers: {},
+            session,
+            body: makeTextResponseBody('')
+          };
+        } catch (error) {
+          return {
+            code: 400,
+            headers: {},
+            session: request.session,
+            body: makeTextResponseBody('')
+          };
+        }
+      })
+    });
+  }
+  return router;
+}
+
+// Process claims into a user, and establish a session
+// If something goes wrong return null
+async function establishSessionWithClaims(connection: Connection, request: Request<any, Session>, tokenSet: TokenSet) {
+  const claims = tokenSet.claims();
+  let userType: UserType;
+  const identityProvider = getString(claims, 'loginSource');
+  switch (identityProvider) {
+    case 'IDIR':
+      const roles = getStringArray(claims, 'roles');
+      if (roles.includes('dm_admin')) {
+        userType = UserType.Admin;
+      } else {
+        userType = UserType.Government;
+      }
+      break;
+    case 'GITHUB':
+      userType = UserType.Vendor;
+      break;
+    default:
+      request.logger.error('unknown identity provider', makeErrorResponseBody(new Error(identityProvider)));
+      makeAuthErrorRedirect(request);
+      return null;
+  }
+
+  const username = getString(claims, 'preferred_username');
+
+  if (!username || !tokenSet.access_token || !tokenSet.refresh_token) {
+    throw new Error('authentication failure - invalid claims');
+  }
+
+  const dbResult = await findOneUserByTypeAndUsername(connection, userType, username);
+  if (isInvalid(dbResult)) {
+    makeAuthErrorRedirect(request);
+  }
+  let user = dbResult.value as User | null;
+  const existingUser = !!user;
+  if (!user) {
+    user = getValidValue(await createUser(connection, {
+      type: userType,
+      status: UserStatus.Active,
+      name: claims.name || '',
+      email: claims.email || '',
+      jobTitle: '',
+      idpUsername: claims.preferred_username
+    }), null);
+
+    // If email present, notify of successful account creation
+    if (user && user.email) {
+      userAccountRegistered(user);
+    }
+  } else if (user.status === UserStatus.InactiveByUser) {
+    const { id } = user;
+    const dbResult = await updateUser(connection, { id, status: UserStatus.Active });
+    // // Send notification
+    if (isValid(dbResult)) {
+      accountReactivatedSelf(dbResult.value);
+    }
+  } else if (user.status === UserStatus.InactiveByAdmin) {
+    makeAuthErrorRedirect(request);
+    return null;
+  }
+
+  if (!user) {
+    makeAuthErrorRedirect(request);
+    return null;
+  }
+
+  const result = await createSession(connection, {
+    user: user && user.id,
+    accessToken: tokenSet.refresh_token
+  });
+  if (isInvalid(result)) {
+    makeAuthErrorRedirect(request);
+    return null;
+  }
+
+  return { session: result.value, existingUser };
 }
 
 function makeAuthErrorRedirect(request: Request<any, Session>) {
