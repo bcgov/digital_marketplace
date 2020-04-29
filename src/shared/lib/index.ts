822a7d11c0802e68836b7500a48a09782a93392b
Andrew Sutherland <sutherlanda@gmail.com>
2020-04-29 09:17:39 -0700

    DM-624 adjust formatAmount to pad zeros on left

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -79,7 +79,9 @@ export function formatAmount(amount: number, currency?: string, ordinal?: boolea
     const remainder = remaining % separateBy;
     remaining = Math.floor(remaining / separateBy);
     if (remainder) {
-      formatted = prepend(remainder);
+      let s = String(remainder);
+      while (s.length < baseTenSeparator && remaining > separateBy) { s = '0' + s; }
+      formatted = prepend(s);
     } else {
       formatted = prepend(repeat('0', baseTenSeparator));
     }
