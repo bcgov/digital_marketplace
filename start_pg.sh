b8c5b43f1ed45d73e8870a6a6dac01c440382fa9
gagf01 <francis.gagne@sct.gouv.qc.ca>
2020-06-09 15:48:16 -0400

    Corrections aux scripts de démarrage conteneurs Keycloak et PostgreSQL

--- start_pg.sh
+++ start_pg.sh
@@ -6,10 +6,12 @@ source .env
 
 PG_STATE="$(docker inspect -f '{{.State.Running}}' postgresql_database)"
 
-if [ $PG_STATE = "true" ]; 
+if [ -z "$PG_STATE" ]
+then docker run -d --name postgresql_database -e POSTGRESQL_USER=$DATABASE_USERNAME -e POSTGRESQL_PASSWORD=$DATABASE_PASSWORD -e POSTGRESQL_DATABASE=$DATABASE_NAME -p 5432:5432 postgresql-12-centos7
+elif [ $PG_STATE = "true" ]; 
 then docker stop postgresql_database
      docker start postgresql_database
 elif [ $PG_STATE = "false" ];
 then docker start postgresql_database
-else docker run -d --name postgresql_database -e POSTGRESQL_USER=$DATABASE_USERNAME -e POSTGRESQL_PASSWORD=$DATABASE_PASSWORD -e POSTGRESQL_DATABASE=$DATABASE_NAME -p 5432:5432 postgresql-12-centos7
+else "Erreur dans la détection de l'état du conteneur PostgreSQL."
 fi
