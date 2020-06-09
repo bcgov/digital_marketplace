b00e73516df0e41febf7d427cf21080dd8e47313
gagf01 <francis.gagne@sct.gouv.qc.ca>
2020-06-09 15:48:16 -0400

    Corrections aux scripts de démarrage conteneurs Keycloak et PostgreSQL

--- keycloak/start.sh
+++ keycloak/start.sh
@@ -6,10 +6,12 @@ source .env
 
 KC_STATE="$(docker inspect -f '{{.State.Running}}' keycloak)"
 
-if [ $KC_STATE = "true" ]; 
+if [ -z "$KC_STATE" ]
+then docker run -d --name keycloak -p 8080:8080 -e KEYCLOAK_USER=$KEYCLOAK_ADMIN_USER -e KEYCLOAK_PASSWORD=$KEYCLOAK_ADMIN_PASS -v $(pwd):/tmp quay.io/keycloak/keycloak
+elif [ $KC_STATE = "true" ]; 
 then docker stop keycloak
      docker start keycloak
 elif [ $KC_STATE = "false" ];
 then docker start keycloak
-else docker run -d --name keycloak -p 8080:8080 -e KEYCLOAK_USER=$KEYCLOAK_ADMIN_USER -e KEYCLOAK_PASSWORD=$KEYCLOAK_ADMIN_PASS quay.io/keycloak/keycloak
+else echo "Erreur dans la détection de l'état du conteneur Keycloak."
 fi
