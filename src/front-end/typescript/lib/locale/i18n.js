4a72e7af40efac8d06e3178cbdb6dc7d259bf615
jose01 <erwan.jossic@itq.gouv.qc.ca>
2020-10-07 18:26:10 +0000

    Options de d Persistence et detection de langage

--- src/front-end/typescript/lib/locale/i18n.js
+++ src/front-end/typescript/lib/locale/i18n.js
@@ -1,8 +1,10 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
 import fr from './fr.json';
 import en from './en.json';
- 
+const defaultLanguage = 'fr';
 const resources = {
   en: {
     translation: en
@@ -15,22 +17,38 @@ const resources = {
 i18n
   // connect with React
   .use(initReactI18next)
+  .use(LanguageDetector)
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    debug: true,
+    debug: process.env.NODE_ENV === "development",
     resources,
-    lng: 'en',
+    lng: defaultLanguage,
+    nonExplicitWhitelist: true,
+    load: 'languageOnly', // do not try to load fr-FR or en-US
     fallbackLng: 'en',
     whitelist: ['en', 'fr'],
  
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
- 
-    backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
-      addPath: '/locales/add/{{lng}}/{{ns}}',
-    },
+
+    detection: {
+      // order and from where user language should be detected
+      order: ['querystring', 'cookie', 'localStorage', 'sessionStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
+
+      // keys or params to lookup language from
+      lookupQuerystring: 'lng',
+      lookupCookie: 'i18next',
+      lookupLocalStorage: 'i18nextLng',
+      lookupSessionStorage: 'i18nextLng',
+      lookupFromPathIndex: 0,
+      lookupFromSubdomainIndex: 0,
+
+      // cache user language on
+      useCookie: true,
+      caches: ['localStorage', 'cookie'],
+      excludeCacheFor: ['cimode'], // languages to not persist (cookie, localStorage)
+    }
   });
  
 export default i18n;
