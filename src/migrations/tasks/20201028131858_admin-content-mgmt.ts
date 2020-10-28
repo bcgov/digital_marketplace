d1fbacacca90807d0e9f4eeabda21f7ba0a63e44
Andrew Sutherland <sutherlanda@gmail.com>
2020-10-28 13:45:27 -0700

    DM-232 add db migration for content tables

---
+++ src/migrations/tasks/20201028131858_admin-content-mgmt.ts
@@ -0,0 +1,36 @@
+import { makeDomainLogger } from 'back-end/lib/logger';
+import { console as consoleAdapter } from 'back-end/lib/logger/adapters';
+import Knex from 'knex';
+
+const logger = makeDomainLogger(consoleAdapter, 'migrations', 'development');
+
+export async function up(connection: Knex): Promise<void> {
+  await connection.schema.createTable('content', table => {
+    table.uuid('id').primary().unique().notNullable();
+    table.timestamp('createdAt').notNullable();
+    table.uuid('createdBy').references('id').inTable('users').notNullable();
+    table.text('slug').unique().notNullable();
+    table.boolean('fixed').notNullable().defaultTo(false);
+  });
+
+  logger.info('Completed creating content table.');
+
+  await connection.schema.createTable('contentVersions', table => {
+    table.integer('id').notNullable();
+    table.text('title').notNullable();
+    table.text('body');
+    table.timestamp('createdAt').notNullable();
+    table.uuid('createdBy').references('id').inTable('users').notNullable();
+    table.uuid('contentId').references('id').inTable('content').notNullable();
+    table.primary(['id', 'contentId']);
+  });
+
+  logger.info('Completed creating contentVersions table.');
+}
+
+export async function down(connection: Knex): Promise<void> {
+  await connection.schema.dropTable('contentVersions');
+  logger.info('Completed dropping contentVersions table.');
+  await connection.schema.dropTable('content');
+  logger.info('Completed dropping content table.');
+}
