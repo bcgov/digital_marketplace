b8698ab6a7db74b8780b4d6d731d1fbd58cfe0e4
Andrew Sutherland <sutherlanda@gmail.com>
2020-10-29 13:40:29 -0700

    DM-232 update slug validation with regex

--- src/shared/lib/validation/content.ts
+++ src/shared/lib/validation/content.ts
@@ -1,10 +1,6 @@
 import { invalid, valid, validateGenericString, Validation } from 'shared/lib/validation';
 import { isBoolean } from 'util';
 
-export function validateSlug(raw: string): Validation<string> {
-  return validateGenericString(raw, 'Slug', 1);
-}
-
 export function validateTitle(raw: string): Validation<string> {
   return validateGenericString(raw, 'Title', 1);
 }
@@ -15,4 +11,12 @@ export function validateBody(raw: string): Validation<string> {
 
 export function validateFixed(raw: any): Validation<boolean> {
   return isBoolean(raw) ? valid(raw) : invalid(['Invalid value for fixed provided.']);
+}
+
+export function validateSlug(raw: string): Validation<string> {
+  if (!raw.match(/^[a-z0-9]+(?:-[a-z0-9]+)*$/)) {
+    return invalid([ 'Please enter a valid slug.' ]);
+  } else {
+    return valid(raw);
+  }
 }
