90a1844026b82cec32b01f04a6f7258b8753da94
Brianna Cerkiewicz <briannac@shaw.ca>
2021-10-27 11:33:57 -0700

    refactor: add if statment to entrypoint to handle different environments

--- src/front-end/docker-entrypoint.sh
+++ src/front-end/docker-entrypoint.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-# to avoid git picking up file permission changes when attaching to Docker container via VS Code
+
 chown -R node:root /usr/app
-git config core.filemode false
 npm install
-npm run migrations:latest && npm run front-end:watch & npm run back-end:watch
+
+if [ "${NODE_ENV}" != "development" ]
+    then
+        NODE_ENV=production npm run front-end:build
+        npm run back-end:build
+        npm start
+
+    else
+        # to avoid git picking up file permission changes when attaching to Docker container via VS Code
+        git config core.filemode false
+        npm run migrations:latest && npm run front-end:watch & npm run back-end:watch
+fi
+
