a1df260afe590f842c0ca08cba3e693231dea03e
bdolor <brad@bradpayne.ca>
2021-11-09 16:08:31 -0800

    move npm install back to dockerfile

--- src/front-end/docker-entrypoint.sh
+++ src/front-end/docker-entrypoint.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-npm install
-chown -R node:root /usr/app
 
 if [ "${NODE_ENV}" != "development" ]
     then
-        NODE_ENV=production npm run front-end:build
-        npm run back-end:build
         npm start
-
     else
         # to avoid git picking up file permission changes when attaching to Docker container via VS Code
         git config core.filemode false
