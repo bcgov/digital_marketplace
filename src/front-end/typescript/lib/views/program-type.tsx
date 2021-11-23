9dbb8a2c14e29474e48e56d7d6173875bfcba0c3
Brianna Cerkiewicz <briannac@shaw.ca>
2021-11-22 16:45:31 -0800

    feat: add to types and update props to accomodate new card

--- src/front-end/typescript/lib/views/program-type.tsx
+++ src/front-end/typescript/lib/views/program-type.tsx
@@ -2,7 +2,7 @@ import { View } from 'front-end/lib/framework';
 import Icon from 'front-end/lib/views/icon';
 import React from 'react';
 
-export type ProgramType = 'cwu' | 'swu';
+export type ProgramType = 'cwu' | 'swu' | 'new';
 
 export interface Props {
   type_: ProgramType;
@@ -11,6 +11,13 @@ export interface Props {
 }
 
 const ProgramType: View<Props> = ({ type_, className = '', size = 'md' }) => {
+  const programType = (() => {
+    switch (type_) {
+      case 'cwu': return 'Code With Us';
+      case 'swu': return 'Sprint With Us';
+      case 'new': return 'New Product';
+    }
+  })();
   const sizeClassName = (() => {
     switch (size) {
       case 'sm': return 'font-size-small';
@@ -25,14 +32,22 @@ const ProgramType: View<Props> = ({ type_, className = '', size = 'md' }) => {
       case 'lg': return 1.2;
     }
   })();
+  const iconType = (() => {
+    switch (type_) {
+      case 'cwu': return 'code';
+      case 'swu': return 'users-class';
+      case 'new': return 'code';
+    }
+  })();
+
   return (
     <div className={`d-flex flex-nowrap align-items-center font-weight-bold text-info ${sizeClassName} ${className}`}>
       <Icon
         className='mr-2 flex-shrink-0 flex-grow-0'
         width={iconSize}
         height={iconSize}
-        name={type_ === 'cwu' ? 'code' : 'users-class'} />
-      {type_ === 'cwu' ? 'Code With Us' : 'Sprint With Us'}
+        name={iconType} />
+      {programType}
     </div>
   );
 };
