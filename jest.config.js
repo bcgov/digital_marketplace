f45da078d4edf7984f3332926bab12218155cfcc
Brianna Cerkiewicz <briannac@shaw.ca>
2021-11-26 10:53:53 -0800

    test: add moduleNameMapper to jest config so tests can find files

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    "front-end/(.*)": "<rootDir>/src/front-end/typescript/$1",
+    "shared/(.*)": "<rootDir>/src/shared/$1"
+  },
   globals: {
     'ts-jest': {
       // use the tsconfig in the front-end testing folder (will need to update if we use Jest for back-end testing too)
