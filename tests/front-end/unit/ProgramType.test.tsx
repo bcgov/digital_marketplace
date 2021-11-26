3b50546e493d6109612628895cd3c9fd0ef7499a
Brianna Cerkiewicz <briannac@shaw.ca>
2021-11-26 15:07:15 -0800

    test: create example ProgramType unit test

--- tests/front-end/unit/ProgramType.test.tsx
+++ tests/front-end/unit/ProgramType.test.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import {render} from '@testing-library/react'
-// "front-end" aliases "front-end/typescript" per the front-end tsconfig, so have to use the same alias here
-import ProgramType from 'front-end/lib/views/program-type'
+// "front-end" aliases "front-end/typescript" per the front-end tsconfig, so have to use the same alias here. IDE might show a TS error but test should run.
+import ProgramType from '../../src/front-end/lib/views/program-type'
+
+
+jest.mock('front-end/lib/framework')
+jest.mock('front-end/lib/views/icon', () => () => (<div>Mocked Icon</div>))
 
 describe('ProgramType contains the correct text based on prop type_', () => {
     it("has correct text for CWU opportunity", () => {
@@ -16,5 +20,11 @@ describe('ProgramType contains the correct text based on prop type_', () => {
             <ProgramType type_='swu'  />
           );
           expect(getByText("Sprint With Us")).toBeTruthy();
+      });
+      it("has correct text for New Product opportunity", () => {
+        const { getByText} = render(
+            <ProgramType type_='new'  />
+          );
+          expect(getByText("New Product")).toBeTruthy();
       });
     })
