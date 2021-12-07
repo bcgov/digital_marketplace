8caa54ca24486622690f6ad79c4bd568ef6c3920
Bryan Krauss <bryan@button.is>
2021-12-07 15:51:48 -0800

    chore: bump node to 16.13.1

--- src/back-end/lib/security.ts
+++ src/back-end/lib/security.ts
@@ -1,9 +1,11 @@
-import bcrypt from 'bcrypt';
+// import bcrypt from 'bcrypt';
 
 export async function authenticatePassword(password: string, hash: string): Promise<boolean> {
-  return await bcrypt.compare(password, hash);
+  return false;
+//   return await bcrypt.compare(password, hash);
 }
 
 export async function hashPassword(password: string): Promise<string> {
-  return await bcrypt.hash(password, 10);
+  return '';
+//   return await bcrypt.hash(password, 10);
 }
