5b17eddf408dfcefb2489ab29e5f47a4123b090a
Brianna Cerkiewicz <brianna@button.is>
2021-12-13 13:26:42 -0800

    fix: fix test error

--- tests/front-end/unit/ProgramType.test.tsx
+++ tests/front-end/unit/ProgramType.test.tsx
@@ -3,13 +3,14 @@ import {render} from '@testing-library/react'
 // "front-end" means the path "front-end/typescript" per the front-end tsconfig, so have to use the same alias here. IDE might show a TS error but test should run.
 import ProgramType from '../../src/front-end/lib/views/program-type'
 
-const MockedIcon = function() {
-  return (<div>Mocked Icon</div>)
-}
 
 
 jest.mock('front-end/lib/framework')
-jest.mock('front-end/lib/views/icon', () => MockedIcon)
+jest.mock('front-end/lib/views/icon', () =>
+function MockedIcon() {
+  return (<div>Mocked Icon</div>)
+}
+)
 
 describe('ProgramType contains the correct text based on prop type_', function() {
     it("has correct text for CWU opportunity", function() {
