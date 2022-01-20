92e37f14a8ad43b1401fe1b1097417a58086c277
Brianna Cerkiewicz <brianna@button.is>
2022-01-20 13:54:47 -0800

    chore: remove code for proposal history notes

--- src/front-end/typescript/lib/pages/proposal/code-with-us/lib/index.ts
+++ src/front-end/typescript/lib/pages/proposal/code-with-us/lib/index.ts
@@ -31,7 +31,6 @@ export function cwuProposalStatusToTitleCase(s: CWUProposalStatus, viewerUserTyp
 export function cwuProposalEventToTitleCase(e: CWUProposalEvent): string {
   switch (e) {
     case CWUProposalEvent.ScoreEntered  : return 'Score Entered';
-    case CWUProposalEvent.NoteAdded     : return 'Note Added';
   }
 }
 
