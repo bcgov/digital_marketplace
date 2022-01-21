acfef123ed7c21adf9cc9f64938213213251304a
Brianna Cerkiewicz <brianna@button.is>
2022-01-21 08:39:17 -0800

    feat: spec work on opportunity note

--- src/front-end/typescript/lib/pages/opportunity/code-with-us/edit/tab/history.tsx
+++ src/front-end/typescript/lib/pages/opportunity/code-with-us/edit/tab/history.tsx
@@ -8,21 +8,47 @@ import React from 'react';
 import { Col, Row } from 'reactstrap';
 import { adt, ADT } from 'shared/lib/types';
 import { isVendor} from 'shared/lib/resources/user';
-import  { iconLinkSymbol, leftPlacement, routeDest } from 'front-end/lib/views/link';
+import  { iconLinkSymbol, leftPlacement } from 'front-end/lib/views/link';
+import { AffiliationMember } from 'shared/lib/resources/affiliation';
+import * as ShortText from 'front-end/lib/components/form-field/short-text';
+import * as FormField from 'front-end/lib/components/form-field';
+import Icon from 'front-end/lib/views/icon';
+
+type ModalId
+  = ADT<'addTeamMembers'>
+  | ADT<'viewTeamMember', AffiliationMember>
+  | ADT<'removeTeamMember', AffiliationMember>
+  | ADT<'approveAffiliation', AffiliationMember>;
 
 export interface State extends Tab.Params {
   history: Immutable<History.State>;
+  showModal: ModalId | null;
+  addTeamMembersEmails: Array<Immutable<ShortText.State>>;
 }
 
 export type InnerMsg
-  = ADT<'history', History.Msg>;
+  = ADT<'history', History.Msg>
+  | ADT<'addTeamMembersEmailsAddField'>;
 
 export type Msg = GlobalComponentMsg<InnerMsg, Route>;
+
+async function initAddTeamMemberEmailField(): Promise<Immutable<ShortText.State>> {
+  return immutable(await ShortText.init({
+    errors: [],
+    child: {
+      id: 'organization-team-add-team-members-emails',
+      type: 'email',
+      value: ''
+    }
+  }));
+}
 
 
 const init: Init<Tab.Params, State> = async params => {
   return {
     ...params,
+    addTeamMembersEmails: [await initAddTeamMemberEmailField()],
+    showModal: null,
     history: immutable(await History.init({
       idNamespace: 'cwu-opportunity-history',
       items: opportunityToHistoryItems(params.opportunity),
@@ -70,15 +96,94 @@ export const component: Tab.Component<State, Msg> = {
   view,
   getContextualActions({ state }) {
     if (!state.viewerUser || isVendor(state.viewerUser)) { return null; }
-    return adt('links', [
-      {
-        children: 'Add Entry',
-        button: true,
-        // disabled: isLoading(state),
-        color: 'primary' as const,
-        symbol_: leftPlacement(iconLinkSymbol('file-export')),
-        dest: routeDest(adt('opportunityCreate', null))
+    return adt('links', [{
+      children: 'Add Team Member(s)',
+      onClick: () => dispatch(adt('showModal', adt('addTeamMembers')) as Msg),
+      button: true,
+      // loading: isAddTeamMembersLoading,
+      // disabled: isLoading,
+      symbol_: leftPlacement(iconLinkSymbol('user-plus')),
+      color: 'primary'
+    }]);
+  },
+
+  getModal: state => {
+    if (!state.showModal) { return null; }
+    switch (state.showModal.tag) {
+      case 'addTeamMembers': {
+        const isValid = true; //check if note meets requirements
+        return {
+          title: 'Add Team Member(s)',
+          onCloseMsg: adt('hideModal'),
+          body: dispatch => {
+            const addField = () => dispatch(adt('addTeamMembersEmailsAddField'));
+            return (
+              <div>
+                <p>Provide an email address for each team member to invite them to join your organization.</p>
+                <p><strong>Please ensure team members have already signed up for a Digital Marketplace Vendor account before adding them to your organization, and only enter the email addresses associated with their Digital Marketplace accounts.</strong></p>
+                {state.addTeamMembersEmails.map((s, i) => {
+                  const isFirst = i === 0;
+                  const isLast = i === state.addTeamMembersEmails.length - 1;
+                  const props = {
+                    extraChildProps: {},
+                    className: 'flex-grow-1 mb-0',
+                    placeholder: 'Email Address',
+
+                    dispatch: mapComponentDispatch(dispatch, v => adt('addTeamMembersEmails', [i, v]) as Msg),
+                    state: s
+                  };
+                  return (
+                    <div key={`organization-add-team-member-email-${i}`}>
+                      {isFirst
+                        ? (<FormField.ConditionalLabel label='Email Addresses' required {...props} />)
+                        : null}
+                      <div className='mb-3 d-flex align-items-start flex-nowrap'>
+                        <ShortText.view {...props} />
+                        <div className='d-flex flex-nowrap align-items-center' style={{ marginTop: '0.625rem' }}>
+                          {state.addTeamMembersEmails.length === 1
+                            ? null
+                            : (<Icon
+                              hover
+                              name='trash'
+                              color='info'
+                              className='ml-2'
+                              width={0.9}
+                              height={0.9}
+
+                              onClick={() => dispatch(adt('addTeamMembersEmailsRemoveField', i))} />)}
+                          <Icon
+                            hover={isLast}
+                            name='plus'
+                            color='primary'
+                            className={`ml-2 ${isLast ? 'o-100' : 'o-0'}`}
+                            width={1.1}
+                            height={1.1}
+                            onClick={isLast ? addField : undefined} />
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          },
+          actions: [
+            {
+              text: 'Add Team Member(s)',
+              button: true,
+              disabled: !isValid,
+              color: 'primary',
+              icon: 'user-plus',
+              msg: adt('addTeamMembers')
+            },
+            {
+              text: 'Cancel',
+              color: 'secondary',
+              msg: adt('hideModal')
+            }
+          ]
+        };
       }
-    ]);
-  }
+    }
+  },
 };
