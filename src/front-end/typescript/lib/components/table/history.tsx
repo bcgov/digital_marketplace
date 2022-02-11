a0c0595f9a8ac65b9e89e9f8903aad17e1f4c6d1
Brianna Cerkiewicz <brianna@button.is>
2022-02-11 11:44:09 -0800

    chore: clean-up

--- src/front-end/typescript/lib/components/table/history.tsx
+++ src/front-end/typescript/lib/components/table/history.tsx
@@ -9,6 +9,7 @@ import { formatDate, formatTime } from 'shared/lib';
 import { isAdmin, User, UserSlim } from 'shared/lib/resources/user';
 import { ADT, adt } from 'shared/lib/types';
 import { AttachmentList } from 'front-end/lib/components/attachments';
+import { FileRecord } from 'shared/lib/resources/file';
 
 export interface Item {
   type: {
@@ -16,7 +17,7 @@ export interface Item {
     color?: ThemeColor;
   };
   note?: string;
-  attachments?: any; // brianna--change type later
+  attachments?: any;
   createdAt: Date;
   createdBy?: UserSlim;
 }
@@ -92,7 +93,6 @@ function tableHeadCells(state: Immutable<State>): Table.HeadCells {
     }
   ];
 }
-
 
 function tableBodyRows(state: Immutable<State>): Table.BodyRows {
   return state.items.map(item => {
