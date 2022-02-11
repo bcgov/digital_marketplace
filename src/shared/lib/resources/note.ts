17afb8919d24e27cd386769bdf8ff1f1afb36636
Brianna Cerkiewicz <brianna@button.is>
2022-02-11 14:28:25 -0800

    chore: clean-up

--- src/shared/lib/resources/note.ts
+++
@@ -1,9 +0,0 @@
-import { UserSlim } from 'shared/lib/resources/user';
-import { Id } from 'shared/lib/types';
-
-export interface Note {
-  id: Id;
-  createdAt: Date;
-  createdBy?: UserSlim;
-  description: string;
-}
