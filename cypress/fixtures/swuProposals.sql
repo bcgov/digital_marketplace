ae335c0795ec319a30a7f3f13e9938aadcb34075
Brianna Cerkiewicz <brianna@button.is>
2022-02-22 13:47:42 -0800

    test: update swu proposal fixtures with new prices

--- cypress/fixtures/swuProposals.sql
+++ cypress/fixtures/swuProposals.sql
@@ -30,9 +30,9 @@ INSERT INTO "swuTeamQuestionResponses" (proposal, "order", response, score) VALU
 INSERT INTO "swuProposals" (id, "createdAt", "createdBy", "updatedAt", "updatedBy", "challengeScore", "scenarioScore", "priceScore", opportunity, organization, "anonymousProponentName") VALUES('7a6dce2b-8344-41aa-a429-43e80758aef2', '2022-02-18 18:59:35.676+00', '06172f56-f08c-4380-86db-603bec98a8c7', '2022-02-18 18:59:35.676+00', '06172f56-f08c-4380-86db-603bec98a8c7', null, null, null, '593f191a-8c7d-4a3b-92e2-4703a80f4f05', '0e593592-4df5-412c-bd68-cc7718e3bc75','');
 
 
-INSERT INTO "swuProposalPhases" (id, proposal, phase, "proposedCost") VALUES('2da3af93-46ae-4749-948b-1e85f3c21180', '7a6dce2b-8344-41aa-a429-43e80758aef2', 'INCEPTION', '100000');
-INSERT INTO "swuProposalPhases" (id, proposal, phase, "proposedCost") VALUES('7e27666c-1362-4aeb-91c5-421e73075d0b', '7a6dce2b-8344-41aa-a429-43e80758aef2', 'PROTOTYPE', '100000');
-INSERT INTO "swuProposalPhases" (id, proposal, phase, "proposedCost") VALUES('6fb5b831-7f56-4da4-8ea6-b39cd3dc4de2', '7a6dce2b-8344-41aa-a429-43e80758aef2', 'IMPLEMENTATION', '10000');
+INSERT INTO "swuProposalPhases" (id, proposal, phase, "proposedCost") VALUES('2da3af93-46ae-4749-948b-1e85f3c21180', '7a6dce2b-8344-41aa-a429-43e80758aef2', 'INCEPTION', '200000');
+INSERT INTO "swuProposalPhases" (id, proposal, phase, "proposedCost") VALUES('7e27666c-1362-4aeb-91c5-421e73075d0b', '7a6dce2b-8344-41aa-a429-43e80758aef2', 'PROTOTYPE', '200000');
+INSERT INTO "swuProposalPhases" (id, proposal, phase, "proposedCost") VALUES('6fb5b831-7f56-4da4-8ea6-b39cd3dc4de2', '7a6dce2b-8344-41aa-a429-43e80758aef2', 'IMPLEMENTATION', '20000');
 
 
 INSERT INTO "swuProposalReferences" (proposal, "order", name, company, phone, email) VALUES('7a6dce2b-8344-41aa-a429-43e80758aef2', '0', 'Ref 1', 'Company 1', '111111111', 'one@email.com');
