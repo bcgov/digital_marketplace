84041036f6f4b11c42097da3c19dd19ae2a698ad
VB <vladimir@acclaim.consulting>
2022-03-30 08:36:02 -0700

    Merge branch '213-setup-deployment' into 213-setup
    # Conflicts:
    #	.github/actions/action.yaml
    #	.github/workflows/publish_deploy_image.yml
    #	Dockerfile
    #	helm/Chart.yaml
    #	helm/templates/cookieSecret.yaml
    #	helm/templates/dbSecret.yaml
    #	helm/templates/deploy.yaml
    #	helm/templates/networkPolicies/networkPolicy.yaml
    #	helm/templates/routes/route.yaml
    #	helm/values.yaml
    #	src/back-end/index.ts

--- src/back-end/index.ts
+++ src/back-end/index.ts
@@ -1,4 +1,4 @@
-import { BASIC_AUTH_PASSWORD_HASH, BASIC_AUTH_USERNAME, DB_MIGRATIONS_TABLE_NAME, ENV, getConfigErrors, KNEX_DEBUG, POSTGRES_URL, SCHEDULED_DOWNTIME, SERVER_HOST, SERVER_PORT } from 'back-end/config';
+import { BASIC_AUTH_PASSWORD_HASH, BASIC_AUTH_USERNAME, DB_MIGRATIONS_TABLE_NAME, ENV, getConfigErrors, KNEX_DEBUG, PG_CONFIG, SCHEDULED_DOWNTIME, SERVER_HOST, SERVER_PORT } from 'back-end/config';
 import * as crud from 'back-end/lib/crud';
 import { Connection, readOneSession } from 'back-end/lib/db';
 import codeWithUsHook from 'back-end/lib/hooks/code-with-us';
@@ -32,8 +32,7 @@ import statusRouter from 'back-end/lib/routers/status';
 import { addHooksToRoute, makeErrorResponseBody, namespaceRoute, notFoundJsonRoute, Route, RouteHook, Router } from 'back-end/lib/server';
 import { express, ExpressAdapter } from 'back-end/lib/server/adapters';
 import { FileUploadMetadata, SupportedRequestBodies, SupportedResponseBodies } from 'back-end/lib/types';
-import Knex from 'knex';
-import { createLightship } from 'lightship';
+import Knex, { ConnectionConfig } from 'knex';
 import { concat, flatten, flow, map } from 'lodash/fp';
 import { flipCurried } from 'shared/lib';
 import { MAX_MULTIPART_FILES_SIZE, parseFilePermissions } from 'shared/lib/resources/file';
@@ -48,10 +47,20 @@ type AppRouter = Router<SupportedRequestBodies, any, any, any, SupportedResponse
 
 const logger = makeDomainLogger(consoleAdapter, 'back-end', ENV);
 
-export function connectToDatabase(postgresUrl: string): Connection {
+// export function connectToDatabase(postgresUrl: string): Connection {
+//   return Knex({
+//     client: 'pg',
+//     connection: postgresUrl,
+//     migrations: {
+//       tableName: DB_MIGRATIONS_TABLE_NAME
+//     },
+//     debug: KNEX_DEBUG
+//   });
+// }
+export function connectToDatabase(connectionConfig: string | ConnectionConfig): Connection {
   return Knex({
     client: 'pg',
-    connection: postgresUrl,
+    connection: connectionConfig,
     migrations: {
       tableName: DB_MIGRATIONS_TABLE_NAME
     },
@@ -141,16 +150,20 @@ export async function createDowntimeRouter(): Promise<AppRouter> {
   ])([]);
 }
 
-async function start(lightship) {
+async function start() {
   // Ensure all environment variables are specified correctly.
   const configErrors = getConfigErrors();
-  if (configErrors.length || !POSTGRES_URL) {
+  if (configErrors.length || !PG_CONFIG) {
     configErrors.forEach((error: string) => logger.error(error));
     throw new Error('Invalid environment variable configuration.');
   }
   // Connect to Postgres.
-  const connection = connectToDatabase(POSTGRES_URL);
-  logger.info('connected to the database');
+  const connection = connectToDatabase(PG_CONFIG);
+  // Test DB connection
+  connection.raw('SELECT 1').then(() => {
+    console.log('PostgreSQL connected');
+});
+
   // Create the router.
   let router: AppRouter = await (SCHEDULED_DOWNTIME ? createDowntimeRouter : createRouter)(connection);
   // Add the status router.
@@ -193,11 +206,10 @@ async function start(lightship) {
       return parseFilePermissions(raw);
     }
   });
-  lightship.signalReady();
   logger.info('server started', { host: SERVER_HOST, port: String(SERVER_PORT) });
 }
-const lightship = createLightship();
-start(lightship)
+
+start()
   .catch(error => {
     logger.error('app startup failed', makeErrorResponseBody(error).value);
     process.exit(1);
