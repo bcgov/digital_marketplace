e9ee4c2de7cba666046be6f206adc61c91afa9b8
Andrew Sutherland <sutherlanda@gmail.com>
2022-10-19 10:14:52 -0600

    chore: update copy for competition number in SWU

--- src/front-end/typescript/lib/pages/opportunity/sprint-with-us/view.tsx
+++ src/front-end/typescript/lib/pages/opportunity/sprint-with-us/view.tsx
@@ -183,6 +183,12 @@ const Header: ComponentView<ValidState, Msg> = ({ state, dispatch }) => {
   const isAcceptingProposals = isSWUOpportunityAcceptingProposals(
     state.opportunity
   );
+  const compNumber =
+    opp.publishedAt &&
+    opp.publishedAt.valueOf() < new Date("2022-10-20").valueOf()
+      ? "ON-003166"
+      : "ON-003166-1";
+
   return (
     <div>
       <Container>
@@ -208,7 +214,7 @@ const Header: ComponentView<ValidState, Msg> = ({ state, dispatch }) => {
               ]}
             />
             <p className="font-italic small text-secondary mb-5">
-              This RFP is a Competition Notice under RFQ No. ON-003166 and is
+              This RFP is a Competition Notice under RFQ No. {compNumber} and is
               restricted to Proponents that have become Qualified Suppliers
               pursuant to that RFQ.
             </p>
