d7ca874db8f4c5143aca619ccc62b26797a9a306
Dhruv Dang <dhruv@realfolk.com>
2022-12-05 13:18:55 -0800

    feat: implement 02-single-counter solution for learning the front-end framework

--- docs/learn/front-end/typescript/index.ts
+++ docs/learn/front-end/typescript/index.ts
@@ -1,4 +1,4 @@
-import * as exercise from "learn-front-end/solutions/01-hello-world";
+import * as exercise from "learn-front-end/solutions/02-single-counter";
 import { process } from "front-end/lib/framework";
 
 const element = document.getElementById("main") || document.body;
