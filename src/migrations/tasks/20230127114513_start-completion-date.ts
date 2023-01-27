e91b24a46ac0a1683653dbd8122c0962cd11b9a6
bdolor <brad@bradpayne.ca>
2023-01-27 12:28:35 -0800

    fix: add default values for start and completion date

--- src/migrations/tasks/20230127114513_start-completion-date.ts
+++ src/migrations/tasks/20230127114513_start-completion-date.ts
@@ -3,19 +3,18 @@ import { console as consoleAdapter } from "back-end/lib/logger/adapters";
 import Knex from "knex";
 
 const logger = makeDomainLogger(consoleAdapter, "migrations", "development");
+const now = new Date().toDateString();
 
-// eslint-disable-next-line no-empty
 export async function up(connection: Knex): Promise<void> {
   await connection.schema.alterTable("twuOpportunityVersions", (table) => {
-    table.timestamp("startDate").notNullable();
-    table.timestamp("completionDate").notNullable();
+    table.timestamp("startDate").defaultTo(now).notNullable();
+    table.timestamp("completionDate").defaultTo(now).notNullable();
   });
   logger.info(
     "Altered twuOpportunityVersions table by adding startDate and completionDate columns."
   );
 }
 
-// eslint-disable-next-line no-empty
 export async function down(connection: Knex): Promise<void> {
   await connection.schema.alterTable("twuOpportunityVersions", (table) => {
     table.dropColumn("startDate");
