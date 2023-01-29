947e95097881c1570cfdd04737c0b49de058a6d6
Andrew Sutherland <sutherlanda@gmail.com>
2023-01-28 21:59:00 -0600

    chore: set logger in info level. Output memory usage

--- src/back-end/lib/hooks/logger.ts
+++ src/back-end/lib/hooks/logger.ts
@@ -2,19 +2,47 @@ import { RouteHook } from "back-end/lib/server";
 import chalk from "chalk";
 import { Session } from "shared/lib/resources/session";
 
-const hook: RouteHook<unknown, unknown, unknown, unknown, number, Session> = {
+interface MemoryUsage {
+  rss: number;
+  heapTotal: number;
+  heapUsed: number;
+  external: number;
+  arrayBuffers: number;
+}
+
+const hook: RouteHook<
+  unknown,
+  unknown,
+  unknown,
+  unknown,
+  [MemoryUsage, number],
+  Session
+> = {
   async before(request) {
-    request.logger.debug(
+    const heap = process.memoryUsage();
+    request.logger.info(
       `${chalk.gray("->")} ${request.method} ${request.path}`,
       { sessionId: request.session?.id || "anonymous" }
     );
-    return Date.now();
+    return [heap, Date.now()];
   },
 
-  async after(startTime, request, response) {
-    request.logger.debug(
+  async after([startMemory, startTime], request, response) {
+    request.logger.info(
       `${chalk.gray("<-")} ${response.code} ${Date.now() - startTime}ms`
     );
+
+    request.logger.info("");
+    for (const key in startMemory) {
+      request.logger.info(
+        `${key} ${
+          Math.round(
+            (startMemory[key as keyof MemoryUsage] / 1024 / 1024) * 100
+          ) / 100
+        } MB`
+      );
+    }
+    request.logger.info("");
   }
 };
 
