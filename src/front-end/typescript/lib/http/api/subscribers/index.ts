c65639106127d7218641210aeaabbf40db7795a1
IanFonzie <ian.fonberg@gov.bc.ca>
2023-01-30 10:24:49 -0800

    feat: add twu opportunity watch endpoints

--- src/front-end/typescript/lib/http/api/subscribers/index.ts
+++ src/front-end/typescript/lib/http/api/subscribers/index.ts
@@ -1,2 +1,3 @@
 export * as cwu from "front-end/lib/http/api/subscribers/code-with-us";
 export * as swu from "front-end/lib/http/api/subscribers/sprint-with-us";
+export * as twu from "front-end/lib/http/api/subscribers/team-with-us";
