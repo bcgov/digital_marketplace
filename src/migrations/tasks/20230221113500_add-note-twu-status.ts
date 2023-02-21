2b4b72f35d9897527b72ad5e135e2737db0907ce
bdolor <brad@bradpayne.ca>
2023-02-21 11:52:30 -0800

    chore: add notes column to twuOpportunityStatuses

---
+++ src/migrations/tasks/20230221113500_add-note-twu-status.ts
@@ -0,0 +1,21 @@
+import { makeDomainLogger } from "back-end/lib/logger";
+import { console as consoleAdapter } from "back-end/lib/logger/adapters";
+import Knex from "knex";
+
+const logger = makeDomainLogger(consoleAdapter, "migrations", "development");
+
+export async function up(connection: Knex): Promise<void> {
+  await connection.schema.alterTable("twuOpportunityStatuses", (table) => {
+    table.text("note");
+  });
+  logger.info("Modified twuOpportunityStatuses table by adding 'note' column.");
+}
+
+export async function down(connection: Knex): Promise<void> {
+  await connection.schema.alterTable("twuOpportunityStatuses", (table) => {
+    table.dropColumn("note");
+  });
+  logger.info(
+    "Reverted twuOpportunityStatuses table by dropping 'note' column."
+  );
+}
