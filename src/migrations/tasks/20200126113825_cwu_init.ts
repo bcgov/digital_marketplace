84f19b314cdaebe99a1098ebbe3ee5a613e97402
bdolor <brad@bradpayne.ca>
2023-02-22 14:07:32 -0800

    fix: expecting two arguments

--- src/migrations/tasks/20200126113825_cwu_init.ts
+++ src/migrations/tasks/20200126113825_cwu_init.ts
@@ -21,7 +21,7 @@ enum CWUProposalStatus {
   Withdrawn = "WITHDRAWN"
 }
 
-const logger = makeDomainLogger(consoleAdapter, "migrations", "development");
+const logger = makeDomainLogger(consoleAdapter, "migrations");
 
 export async function up(connection: Knex): Promise<void> {
   // Create CWUOpportunity table
--- src/migrations/tasks/20200311161600_string_to_text.ts
+++ src/migrations/tasks/20200311161600_string_to_text.ts
@@ -2,7 +2,7 @@ import { makeDomainLogger } from "back-end/lib/logger";
 import { console as consoleAdapter } from "back-end/lib/logger/adapters";
 import Knex from "knex";
 
-const logger = makeDomainLogger(consoleAdapter, "migrations", "development");
+const logger = makeDomainLogger(consoleAdapter, "migrations");
 
 export async function up(connection: Knex): Promise<void> {
   await connection.schema.alterTable("cwuOpportunityAddenda", (table) => {
