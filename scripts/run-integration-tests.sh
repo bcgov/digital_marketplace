b2dcb8c95cd73525f0b18cf948fc102c99b55112
Andrew Sutherland <sutherlanda@gmail.com>
2023-03-06 20:24:28 -0600

    chore: enhance integration test harness

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -5,9 +5,11 @@ export POSTGRES_URL='postgresql://digmkt-test:digmkt-test@localhost:5433/digmkt-
 export NODE_ENV='test'
 export SERVER_HOST="0.0.0.0"
 export SERVER_PORT="3000"
+export LOG_MEM_USAGE="false"
+export LOG_DEBUG="false"
 
 docker compose up -d test-db
 
 yarn migrations:latest
 
-jest --detectOpenHandles --forceExit --config tests/back-end/jest.config.ts
+jest --detectOpenHandles --forceExit --silent --config tests/back-end/jest.config.ts
