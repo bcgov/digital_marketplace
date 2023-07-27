14b92c21e60a107195781dda3a2cfd6741d4a2b5
IanFonzie <ian.fonberg@gov.bc.ca>
2023-07-27 16:24:31 -0700

    feat: add twu proposal object mothers

---
+++ tests/utils/generate/proposals/team-with-us.ts
@@ -0,0 +1,74 @@
+import {
+  TWUProposal,
+  TWUProposalStatus,
+  TWUProposalTeamMember,
+  TWUProposalResourceQuestionResponse
+} from "shared/lib/resources/proposal/team-with-us";
+import { DEFAULT_RESOURCE_QUESTION_RESPONSE_WORD_LIMIT } from "shared/lib/resources/opportunity/team-with-us";
+import { getId } from "..";
+import { fakerEN_CA as faker } from "@faker-js/faker";
+import { buildTWUOpportunitySlim } from "../opportunities/team-with-us";
+import { buildUserSlim } from "../user";
+
+function buildTWUProposal(overrides: Partial<TWUProposal> = {}): TWUProposal {
+  const {
+    createdBy = buildUserSlim(),
+    opportunity = buildTWUOpportunitySlim({ createdAt: faker.date.past() }),
+    createdAt = faker.date.between({
+      from: opportunity.createdAt,
+      to: opportunity.proposalDeadline
+    })
+  } = overrides;
+
+  overrides.organization = overrides.organization
+    ? { ...overrides.organization, owner: createdBy }
+    : overrides.organization;
+
+  return {
+    id: getId(),
+    createdBy,
+    createdAt,
+    updatedBy: createdBy,
+    updatedAt: createdAt,
+    status: TWUProposalStatus.Draft,
+    opportunity: buildTWUOpportunitySlim(),
+    attachments: [],
+    resourceQuestionResponses: [
+      ...Array(faker.number.int({ min: 1, max: 4 }))
+    ].map((_, i) =>
+      buildTWUProposalResourceQuestionResponse({
+        order: i
+      })
+    ),
+    team: [buildTWUProposalTeamMember()],
+    anonymousProponentName: "",
+    ...overrides
+  };
+}
+
+function buildTWUProposalTeamMember(
+  overrides: Partial<TWUProposalTeamMember> = {}
+): TWUProposalTeamMember {
+  return {
+    member: buildUserSlim(),
+    pending: false,
+    idpUsername: faker.internet.userName(),
+    hourlyRate: faker.number.float({ max: 200, precision: 0.01 }),
+    ...overrides
+  };
+}
+
+function buildTWUProposalResourceQuestionResponse(
+  overrides: Partial<TWUProposalResourceQuestionResponse> = {}
+): TWUProposalResourceQuestionResponse {
+  return {
+    response: faker.lorem.words({
+      min: 1,
+      max: DEFAULT_RESOURCE_QUESTION_RESPONSE_WORD_LIMIT
+    }),
+    order: 0,
+    ...overrides
+  };
+}
+
+export { buildTWUProposal, buildTWUProposalResourceQuestionResponse };
