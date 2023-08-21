edfbaf512b25a4353c88650d4519b3191f1a7ae6
IanFonzie <ian.fonberg@gov.bc.ca>
2023-08-21 08:50:23 -0700

    fix: apply timezone to opportunity date helper

--- tests/utils/date.ts
+++ tests/utils/date.ts
@@ -1,5 +1,9 @@
+import { normalizeDateTimezone } from "shared/lib";
+
 function dateAt4PM(date: Date) {
-  return new Date(date.getFullYear(), date.getMonth(), date.getDate(), 16);
+  return normalizeDateTimezone(
+    new Date(date.getFullYear(), date.getMonth(), date.getDate(), 16)
+  ).toDate();
 }
 
 export { dateAt4PM };
