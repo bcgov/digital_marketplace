9af74afccd61f7f5ba134178b77d04d84a729661
IanFonzie <ian.fonberg@gov.bc.ca>
2024-03-01 13:11:12 -0800

    refactor: add order and remove resource

---
+++ src/migrations/tasks/20240229190142_twu-proposal-member-order.ts
@@ -0,0 +1,116 @@
+import { makeDomainLogger } from "back-end/lib/logger";
+import { console as consoleAdapter } from "back-end/lib/logger/adapters";
+import { Knex } from "knex";
+
+const logger = makeDomainLogger(consoleAdapter, "migrations");
+
+export async function up(connection: Knex): Promise<void> {
+  /**
+   * Adds a new column to the twuProposalMember table to store the resource ids
+   */
+  await connection.schema.alterTable("twuProposalMember", function (table) {
+    table.integer("order");
+  });
+
+  logger.info("Added order column to twuProposalMember table");
+
+  /**
+   * Fetch order explicitly from the resource
+   */
+  await connection("twuProposalMember as tpm").update({
+    order: connection("twuResources as tr")
+      .select("order")
+      .where("id", connection.raw("??", ["tpm.resource"]))
+  });
+
+  logger.info(
+    "Added default data (resource ids) to resource column in the twuProposalMember table"
+  );
+
+  /**
+   * after data has been added, we can declare that it be NOT NULL
+   */
+  await connection.schema.alterTable("twuProposalMember", function (table) {
+    table.integer("order").notNullable().alter();
+    table.dropColumn("resource");
+  });
+
+  logger.info("Added NOT NULL property to the column");
+}
+
+export async function down(connection: Knex): Promise<void> {
+  await connection.schema.alterTable("twuProposalMember", function (table) {
+    table.dropColumn("order");
+  });
+
+  /**
+   * Adds a new column to the twuProposalMember table to store the resource ids
+   */
+  await connection.schema.alterTable("twuProposalMember", function (table) {
+    table
+      .uuid("resource")
+      .references("id")
+      .inTable("twuResources")
+      .onDelete("CASCADE");
+  });
+
+  logger.info("Added resource column to twuProposalMember table");
+
+  /**
+   * Obtains records of previously created opportunities and relevant proposals
+   * where there was only one resource per proposal (for TWU). All the
+   * resourceIds are populated in the new column prior to it being declared
+   * as not null.
+   */
+  const records = await connection("twuProposalMember")
+    .select("member.proposal", "resources.id")
+    .from("twuProposalMember as member")
+    .innerJoin("twuProposals as proposal", "member.proposal", "proposal.id")
+    .innerJoin("twuOpportunityVersions as versions", function () {
+      this.on("proposal.opportunity", "versions.opportunity").andOn(
+        "versions.createdAt",
+        connection.raw(`(select max("createdAt")
+                                                   from "twuOpportunityVersions" as versions2
+                                                   where versions2.opportunity = proposal.opportunity)`)
+      );
+    })
+    .innerJoin(
+      "twuResources as resources",
+      "versions.id",
+      "resources.opportunityVersion"
+    );
+
+  /**
+   * iterates through the records and updates the new resource column in the
+   * twuProposalMember table with the relevant resourceId. Prior to this point
+   * in time, there was one and only one ServiceArea/resourceId per TWU
+   * opportunity, so we are guaranteed the resourceId to be correct. This
+   * is done so that the column can later be declared NOT NULL.
+   *
+   * If the one and only one ServiceArea/resourceId per TWU opportunity can NOT
+   * be guaranteed such as in a situation where this migration is performed,
+   * multiple TWU resources are added, saved and proposals are made with multiple
+   * resources, there is less guarantee that the above query being free from
+   * issues.
+   */
+  for (const record of records) {
+    await connection("twuProposalMember")
+      .update({ resource: record.id })
+      .where({ proposal: record.proposal });
+  }
+
+  logger.info(
+    "Added default data (resource ids) to resource column in the twuProposalMember table"
+  );
+
+  /**
+   * after data has been added, we can declare that it be NOT NULL
+   */
+  await connection.schema.alterTable("twuProposalMember", function (table) {
+    table.uuid("resource").notNullable().alter();
+  });
+
+  logger.info("Added NOT NULL property to the column");
+
+  logger.info("Dropped order column in the twuProposalMember table");
+}
