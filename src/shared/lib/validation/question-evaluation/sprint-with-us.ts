da1f815e9608cd2016fa2260faa113ac4e9132a9
IanFonzie <ian.fonberg@gov.bc.ca>
2024-08-02 15:32:43 -0700

    refactor: make enum validation helper

--- src/shared/lib/validation/question-evaluation/sprint-with-us.ts
+++ src/shared/lib/validation/question-evaluation/sprint-with-us.ts
@@ -4,18 +4,29 @@ import {
 } from "shared/lib/resources/question-evaluation/sprint-with-us";
 import { invalid, valid, Validation } from "shared/lib/validation";
 
-export function validateSWUTeamQuestionResponseEvaluationType(
-  raw: string,
-  isOneOf: SWUTeamQuestionResponseEvaluationType[]
-): Validation<SWUTeamQuestionResponseEvaluationType> {
-  const parsed = parseSWUTeamQuestionResponseEvaluationType(raw);
-  if (!parsed) {
-    return invalid([
-      `"${raw}" is not a valid SprintWithUs team question response evaluation type.`
-    ]);
-  }
-  if (!isOneOf.includes(parsed)) {
-    return invalid([`"${raw}" is not one of: ${isOneOf.join(", ")}`]);
-  }
-  return valid(parsed);
+function makeValidateSWUTeamQuestionResponseEvaluationEnum<T>(
+  parse: (raw: string) => T | null,
+  parseErrorMsg: (raw: string) => string,
+  notOneOfErrorMsg: (raw: string, isOneOf: T[]) => string
+): (raw: string, isOneOf: T[]) => Validation<T> {
+  return (raw, isOneOf) => {
+    const parsed = parse(raw);
+    if (!parsed) {
+      return invalid([parseErrorMsg(raw)]);
+    }
+
+    if (!isOneOf.includes(parsed)) {
+      return invalid([notOneOfErrorMsg(raw, isOneOf)]);
+    }
+
+    return valid(parsed);
+  };
 }
+
+export const validateSWUTeamQuestionResponseEvaluationType =
+  makeValidateSWUTeamQuestionResponseEvaluationEnum<SWUTeamQuestionResponseEvaluationType>(
+    parseSWUTeamQuestionResponseEvaluationType,
+    (raw) =>
+      `"${raw}" is not a valid SprintWithUs team question response evaluation type.`,
+    (raw, isOneOf) => `"${raw}" is not one of: ${isOneOf.join(", ")}`
+  );
