e8ad2ad2b155e1fcfb10e8e7dd8d28340732ef9d
IanFonzie <ian.fonberg@gov.bc.ca>
2024-08-09 07:17:06 -0700

    feat: validate swu team question response evaluation scores

--- src/shared/lib/validation/question-evaluation/sprint-with-us.ts
+++ src/shared/lib/validation/question-evaluation/sprint-with-us.ts
@@ -1,10 +1,25 @@
+import { isArray } from "lodash";
+import { getNumber, getString } from "shared/lib";
+import { SWUProposalTeamQuestionResponse } from "shared/lib/resources/proposal/sprint-with-us";
 import {
+  CreateSWUTeamQuestionResponseEvaluationScoreBody,
+  CreateSWUTeamQuestionResponseEvaluationScoreValidationErrors,
   parseSWUTeamQuestionResponseEvaluationStatus,
   parseSWUTeamQuestionResponseEvaluationType,
   SWUTeamQuestionResponseEvaluationStatus,
   SWUTeamQuestionResponseEvaluationType
 } from "shared/lib/resources/question-evaluation/sprint-with-us";
-import { invalid, valid, Validation } from "shared/lib/validation";
+import {
+  ArrayValidation,
+  getInvalidValue,
+  invalid,
+  isInvalid,
+  valid,
+  validateArrayCustom,
+  validateGenericStringWords,
+  validateNumber,
+  Validation
+} from "shared/lib/validation";
 
 function makeValidateSWUTeamQuestionResponseEvaluationEnum<T>(
   parse: (raw: string) => T | null,
@@ -40,3 +55,97 @@ export const validateSWUTeamQuestionResponseEvaluationStatus =
       `"${raw}" is not a valid SprintWithUs team question response evaluation status.`,
     (raw, isOneOf) => `"${raw}" is not one of: ${isOneOf.join(", ")}`
   );
+
+export function validateSWUTeamQuestionResponseEvaluationScoreOrder(
+  raw: number,
+  proposalTeamQuestionResponses: SWUProposalTeamQuestionResponse[]
+): Validation<number> {
+  return validateNumber(raw, 0, proposalTeamQuestionResponses.length, "Order");
+}
+
+export function validateSWUTeamQuestionResponseEvaluationScoreScore(
+  raw: number
+): Validation<number> {
+  return validateNumber(raw, 1, undefined, "Score");
+}
+
+export function validateSWUTeamQuestionResponseEvaluationScoreNotes(
+  raw: string
+): Validation<string> {
+  return validateGenericStringWords(raw, "Notes", 1);
+}
+
+export function validateSWUTeamQuestionResponseEvaluationScore(
+  raw: any,
+  proposalTeamQuestionResponses: SWUProposalTeamQuestionResponse[]
+): Validation<
+  CreateSWUTeamQuestionResponseEvaluationScoreBody,
+  CreateSWUTeamQuestionResponseEvaluationScoreValidationErrors
+> {
+  const validatedOrder = validateSWUTeamQuestionResponseEvaluationScoreOrder(
+    getNumber(raw, "order"),
+    proposalTeamQuestionResponses
+  );
+  if (isInvalid(validatedOrder)) {
+    return invalid({
+      order: getInvalidValue(validatedOrder, undefined)
+    });
+  }
+  const proposalTeamQuestionResponse = proposalTeamQuestionResponses.find(
+    (q) => q.order === validatedOrder.value
+  );
+  if (!proposalTeamQuestionResponse) {
+    return invalid({
+      order: ["No matching proposal team question response."]
+    });
+  }
+  const validatedScore = validateSWUTeamQuestionResponseEvaluationScoreScore(
+    getNumber(raw, "score")
+  );
+  if (isInvalid(validatedScore)) {
+    return invalid({
+      score: getInvalidValue(validatedScore, undefined)
+    });
+  }
+  const validatedNotes = validateSWUTeamQuestionResponseEvaluationScoreNotes(
+    getString(raw, "notes")
+  );
+  if (isInvalid(validatedNotes)) {
+    return invalid({
+      notes: getInvalidValue(validatedNotes, undefined)
+    });
+  } else {
+    return valid({
+      notes: validatedNotes.value,
+      order: validatedOrder.value,
+      score: validatedScore.value
+    } as CreateSWUTeamQuestionResponseEvaluationScoreBody);
+  }
+}
+
+export function validateSWUTeamQuestionResponseEvaluationScores(
+  raw: any,
+  proposalTeamQuestionResponses: SWUProposalTeamQuestionResponse[]
+): ArrayValidation<
+  CreateSWUTeamQuestionResponseEvaluationScoreBody,
+  CreateSWUTeamQuestionResponseEvaluationScoreValidationErrors
+> {
+  if (!isArray(raw)) {
+    return invalid([
+      {
+        parseFailure: [
+          "Please provide an array of team question response evaluations."
+        ]
+      }
+    ]);
+  }
+  return validateArrayCustom(
+    raw,
+    (v) =>
+      validateSWUTeamQuestionResponseEvaluationScore(
+        v,
+        proposalTeamQuestionResponses
+      ),
+    {}
+  );
+}
