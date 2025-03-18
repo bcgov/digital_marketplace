796935558dcca9082f7c9d6a301c410d165c62ea
IanFonzie <ian.fonberg@gov.bc.ca>
2025-03-18 13:41:24 -0700

    refactor: add new consensus http client functions

--- src/front-end/typescript/lib/http/api/proposal/sprint-with-us/team-questions/index.ts
+++ src/front-end/typescript/lib/http/api/proposal/sprint-with-us/team-questions/index.ts
@@ -1,1 +1,2 @@
 export * as evaluations from "front-end/lib/http/api/proposal/sprint-with-us/team-questions/evaluations";
+export * as consensuses from "front-end/lib/http/api/proposal/sprint-with-us/team-questions/consensus";
