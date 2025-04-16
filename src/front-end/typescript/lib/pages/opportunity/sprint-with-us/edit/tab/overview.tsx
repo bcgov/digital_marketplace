383f23beefda286a1fee37aea4842a621e7d2859
IanFonzie <ian.fonberg@gov.bc.ca>
2025-04-16 14:51:03 -0700

    refactor: remove anonymous proponent text

--- src/front-end/typescript/lib/pages/opportunity/sprint-with-us/edit/tab/overview.tsx
+++ src/front-end/typescript/lib/pages/opportunity/sprint-with-us/edit/tab/overview.tsx
@@ -277,27 +277,15 @@ const ProponentCell: component_.base.View<ProponentCellProps> = ({
     tab: "proposal" as const
   };
   return (
-    <div>
-      <Link
-        symbol_={leftPlacement(
-          warn ? iconLinkSymbol("exclamation-circle") : emptyIconLinkSymbol()
-        )}
-        symbolClassName="text-warning"
-        disabled={disabled}
-        dest={routeDest(adt("proposalSWUView", proposalRouteParams))}>
-        {getSWUProponentName(proposal)}
-      </Link>
-      {(() => {
-        if (!proposal.organization) {
-          return null;
-        }
-        return (
-          <div className="small text-secondary text-uppercase">
-            {proposal.anonymousProponentName}
-          </div>
-        );
-      })()}
-    </div>
+    <Link
+      symbol_={leftPlacement(
+        warn ? iconLinkSymbol("exclamation-circle") : emptyIconLinkSymbol()
+      )}
+      symbolClassName="text-warning"
+      disabled={disabled}
+      dest={routeDest(adt("proposalSWUView", proposalRouteParams))}>
+      {getSWUProponentName(proposal)}
+    </Link>
   );
 };
 
